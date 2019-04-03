026bb9e56e645d349766be4275c67b2a9ab63270
AndreyPA <andrey@uma.ru>
2019-04-03 21:57:03 +1000

    fix

--- src/index.php
+++ src/index.php
@@ -1,9 +1,12 @@
 <?php 
-    if ($_POST['string'] == '(()()()()))((((()()()))(()()()(((()))))))') 
-    {
-	echo "всё хорошо";
+    if (empty($_POST)) {
+	header('HTTP/1.1 500 Internal Server Error');
+	echo "The POST required";
+    } else if (isset($_POST['string']) && $_POST['string'] == '(()()()()))((((()()()))(()()()(((()))))))') {
+	header('HTTP/1.1 200 OK');
+	echo "OK";
     } else {
 	header('HTTP/1.1 400 Bad Request');
+	echo "Bad request";
     }
-?>
 
