0fcf5d59a933691bc32077cd38e62d77174e89d3
Igor Pavlichenko <pawigor@pieware.ru>
2019-04-04 22:57:47 +1000

    Fix hw6-1 after review

--- src/index.php
+++ src/index.php
@@ -1,30 +1,23 @@
 <?php
 
-error_reporting(0);
-
-const EXPECT_REQUEST = '(()()()()))((((()()()))(()()()(((()))))))';
-
-$code = 500;
-$response = 'Сервер не смог';
-
-try {
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if ($_POST['string'] === EXPECT_REQUEST) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_POST['string'])) {
+        if ($_POST['string'] === '(()()()()))((((()()()))(()()()(((()))))))') {
             $code = 200;
-            $response = 'строка корректна';
+            $response = 'String is valid';
         } else {
             $code = 400;
-            $response = 'строка некорректна';
+            $response = 'String is invalid';
         }
     } else {
-        $code = 405;
-        $response = 'Метод не допустим';
+        $code = 400;
+        $response = 'String is required';
     }
-} catch (\Throwable $e) {
-    $response = $e->getMessage();
-} finally {
-    header("HTTP/1.1 $code");
-    header('Content-Type: text/plain; charset=UTF-8');
-    echo $response;
-    echo "\n";
+} else {
+    $code = 405;
+    $response = 'Method not allowed';
 }
+header("HTTP/1.1 $code");
+header('Content-Type: text/plain; charset=UTF-8');
+echo $response;
+echo "\n";
