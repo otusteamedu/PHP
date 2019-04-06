71f9daa33d95af324f7850a7f827cbce15ac5ac3
f.pronin <f.pronin@fin.media>
2019-04-06 18:27:07 +0300

    Add error messages

--- app/client.php
+++ app/client.php
@@ -3,5 +3,18 @@
 require 'vendor/autoload.php';
 
 $socket = socket_create(AF_UNIX, SOCK_DGRAM, 0);
-socket_sendto($socket, "Hello World!", 12, 0, "./server.sock", 0);
-echo "sent" . PHP_EOL;
+
+if ($socket === false) {
+    $errorCode = socket_last_error();
+    $errorMsg = socket_strerror($errorCode);
+    die("Не могу создать сокет: [$errorCode] $errorMsg");
+}
+
+$res = socket_sendto($socket, "Hello World!", 12, 0, "./server.sock", 0);
+
+if ($res === false) {
+    die("Отправка не удалась.");
+} else {
+    echo "sent" . PHP_EOL;
+}
+
--- app/server.php
+++ app/server.php
@@ -7,6 +7,13 @@ unlink($file);
 
 $socket = socket_create(AF_UNIX, SOCK_DGRAM, 0);
 
+if ($socket === false) {
+    $errorCode = socket_last_error();
+    $errorMsg = socket_strerror($errorCode);
+    die("Не могу создать сокет: [$errorCode] $errorMsg");
+}
+
+
 if (socket_bind($socket, $file) === false) {
     echo "bind failed" . PHP_EOL;
 }
