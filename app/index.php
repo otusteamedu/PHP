d2b8ec72a11ccf2483ed31f3498c9d8888660b0b
f.pronin <f.pronin@fin.media>
2019-04-07 13:21:11 +0300

    Email service

--- app/index.php
+++ app/index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo 123;
+require 'vendor/autoload.php';
+
+use App\EmailService;
+
+$emails = [
+    'f.pronin@fin.media',
+    'feodor.pronin@gmail.com',
+    'da93jda[3@2d3a33@mail.ru',
+    'da93jda[3@2d3a33/d.dd',
+    'sdsdsda',
+];
+
+foreach ($emails as $email) {
+    echo $email . ' - ';
+    echo EmailService::validateEmail($email) ? 'ok' : 'not valid';
+    echo '<br />';
+}
---
+++ app/src/EmailService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+/**
+ * Class EmailService
+ * @package App
+ */
+class EmailService
+{
+    /**
+     * @param string $email
+     * @return bool
+     */
+    public static function validateEmail(string $email): bool
+    {
+        $regExp = '/^[0-9a-z-\.]+\@[0-9a-z-]{2,}\.[a-z]{2,}$/i';
+        if (!preg_match($regExp, $email)) {
+            return false;
+        }
+
+        $domain = substr(strrchr($email, "@"), 1);
+        $res = getmxrr($domain, $mxRecords, $mxWeight);
+        if (
+            $res === false ||
+            count($mxRecords) === 0 ||
+            (count($mxRecords) === 1 && ($mxRecords[0] === null || $mxRecords[0] === '0.0.0.0'))
+        ) {
+            return false;
+        }
+
+        return true;
+    }
+}
