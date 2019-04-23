4008a15fb081ab2002d380797f3ad408894295d6
Igor Pavlichenko <inbox@pieware.pro>
2019-04-23 17:55:07 +1000

    Fix: change manual create sequences to serial datatype

--- DDL.sql
+++ DDL.sql
@@ -1,21 +1,14 @@
-CREATE SEQUENCE hall_seq;
-
-DROP TABLE IF EXISTS hall;
 
 CREATE TABLE hall
 (
-    id_hall INT          NOT NULL DEFAULT nextval('hall_seq'),
+    id_hall SERIAL,
     name    VARCHAR(512) NOT NULL,
     PRIMARY KEY (id_hall)
 );
 
-ALTER SEQUENCE hall_seq OWNED BY hall.id_hall;
-
-CREATE SEQUENCE film_seq;
-
 CREATE TABLE IF NOT EXISTS film
 (
-    id_film INT           NOT NULL DEFAULT nextval('film_seq'),
+    id_film SERIAL,
     name    VARCHAR(1024) NOT NULL,
     year    CHAR(4)       NOT NULL,
     info    JSON          NULL,
@@ -23,13 +16,9 @@ CREATE TABLE IF NOT EXISTS film
     PRIMARY KEY (id_film)
 );
 
-ALTER SEQUENCE film_seq OWNED BY film.id_film;
-
-CREATE SEQUENCE session_seq;
-
 CREATE TABLE IF NOT EXISTS session
 (
-    id_session INT   NOT NULL DEFAULT nextval('film_seq'),
+    id_session SERIAL,
     date       DATE  NOT NULL,
     time       TIME  NOT NULL,
     id_hall    INT   NOT NULL,
@@ -40,21 +29,15 @@ CREATE TABLE IF NOT EXISTS session
     FOREIGN KEY (id_film) REFERENCES film (id_film)
 );
 
-ALTER SEQUENCE session_seq OWNED BY session.id_session;
-
 CREATE UNIQUE INDEX session_UNIQUE ON session (date ASC, time ASC, id_hall ASC, id_film ASC);
-
-CREATE SEQUENCE client_seq;
 
 CREATE TABLE IF NOT EXISTS "client"
 (
-    id_client INT         NOT NULL DEFAULT nextval('client_seq'),
+    id_client SERIAL,
     name      VARCHAR(45) NOT NULL,
     card      VARCHAR(45) NULL,
     PRIMARY KEY (id_client)
 );
-
-ALTER SEQUENCE client_seq OWNED BY "client".id_client;
 
 CREATE TABLE IF NOT EXISTS client_session
 (
