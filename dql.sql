1c124d1d11cbe50995ba17261eafebc0bf365c47
f.pronin <f.pronin@fin.media>
2019-04-27 21:25:02 +0300

    Fix tables

--- dql.sql
+++ dql.sql
@@ -1,39 +1,32 @@
 create table film (
-    id              serial primary key,
-    name            varchar(255) unique not null,
-    duration        time not null
+    id       serial primary key,
+    name     varchar(255) unique not null,
+    duration time not null
 );
 
 create table hall (
-    id              serial primary key,
-    name            varchar(255) unique not null,
-    seats           integer unique not null
+    id    serial primary key,
+    name  varchar(255) unique not null,
+    seats integer unique not null
 );
 
 create table customer (
-    id              serial primary key,
-    name            varchar(255) unique not null
-);
-
-create table seance_type (
-    id              serial primary key,
-    name            varchar(255) unique not null,
-    seance_time     time not null,
-    price           integer not null
+    id   serial primary key,
+    name varchar(255) unique not null
 );
 
 create table seance (
-    id              serial primary key,
-    film_id         integer not null references film (id),
-    hall_id         integer not null references hall (id),
-    seance_type_id  integer not null references seance_type (id),
-    seance_date     date not null
+    id          serial primary key,
+    film_id     integer not null references film (id),
+    hall_id     integer not null references hall (id),
+    seance_time timestamp(0) not null,
+    price       integer not null
 );
 
 create table ticket (
-    id              serial primary key,
-    customer_id     integer not null references customer (id),
-    seance_id       integer not null references seance (id),
-    purchase_date   timestamp(0) not null,
-    seat            integer not null
+    id            serial primary key,
+    customer_id   integer not null references customer (id),
+    seance_id     integer not null references seance (id),
+    purchase_date timestamp(0) not null,
+    seat          integer not null
 );
--- fixture.sql
+++ fixture.sql
@@ -7,21 +7,16 @@ insert into hall (name, seats) values ('Зал №2', 300);
 insert into customer (name) values ('Федор');
 insert into customer (name) values ('Михаил');
 
-insert into seance_type (name, seance_time, price) values ('Утренний 10', '10:00:00', 100);
-insert into seance_type (name, seance_time, price) values ('Утренний 14', '14:00:00', 150);
-insert into seance_type (name, seance_time, price) values ('Утренний 18', '18:00:00', 250);
-insert into seance_type (name, seance_time, price) values ('Утренний 22', '22:00:00', 350);
-
-insert into seance (film_id, hall_id, seance_type_id, seance_date) values (1, 1, 1, '2019-04-01');
-insert into seance (film_id, hall_id, seance_type_id, seance_date) values (1, 1, 2, '2019-04-01');
-insert into seance (film_id, hall_id, seance_type_id, seance_date) values (1, 1, 3, '2019-04-01');
-insert into seance (film_id, hall_id, seance_type_id, seance_date) values (1, 1, 4, '2019-04-01');
-insert into seance (film_id, hall_id, seance_type_id, seance_date) values (2, 2, 1, '2019-04-01');
-insert into seance (film_id, hall_id, seance_type_id, seance_date) values (2, 2, 2, '2019-04-01');
-insert into seance (film_id, hall_id, seance_type_id, seance_date) values (2, 2, 3, '2019-04-01');
-insert into seance (film_id, hall_id, seance_type_id, seance_date) values (2, 2, 4, '2019-04-01');
+insert into seance (film_id, hall_id, seance_time, price) values (1, 1, '2019-04-01 10:00:00', 100);
+insert into seance (film_id, hall_id, seance_time, price) values (1, 1, '2019-04-01 14:00:00', 150);
+insert into seance (film_id, hall_id, seance_time, price) values (1, 1, '2019-04-01 18:00:00', 200);
+insert into seance (film_id, hall_id, seance_time, price) values (1, 1, '2019-04-01 22:00:00', 300);
+insert into seance (film_id, hall_id, seance_time, price) values (2, 2, '2019-04-02 10:00:00', 100);
+insert into seance (film_id, hall_id, seance_time, price) values (2, 2, '2019-04-02 14:00:00', 150);
+insert into seance (film_id, hall_id, seance_time, price) values (2, 2, '2019-04-02 18:00:00', 200);
+insert into seance (film_id, hall_id, seance_time, price) values (2, 2, '2019-04-02 22:00:00', 300);
 
 insert into ticket (customer_id, seance_id, purchase_date, seat) values (1, 3, '2019-04-01 09:00:00', 1);
-insert into ticket (customer_id, seance_id, purchase_date, seat) values (2, 3, '2019-04-01 09:00:00', 2);
-insert into ticket (customer_id, seance_id, purchase_date, seat) values (1, 8, '2019-04-01 09:00:00', 3);
-insert into ticket (customer_id, seance_id, purchase_date, seat) values (2, 8, '2019-04-01 09:00:00', 4);
+insert into ticket (customer_id, seance_id, purchase_date, seat) values (2, 3, '2019-04-01 09:01:00', 2);
+insert into ticket (customer_id, seance_id, purchase_date, seat) values (1, 8, '2019-04-02 18:00:00', 3);
+insert into ticket (customer_id, seance_id, purchase_date, seat) values (2, 8, '2019-04-02 18:01:00', 4);
