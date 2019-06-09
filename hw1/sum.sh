c8e2e69ffbb949b8b17a20080fbb989678a569b9
Evgeny Prokhorov <prohorov-evgen@ya.ru>
2019-06-09 15:52:21 +0300

    removed variables usage

--- hw1/sum.sh
+++ hw1/sum.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-arg1=$1
-arg2=$2
 
-if [ -z "$arg2" ]
+if [ -z "$2" ]
 then
     echo "Error: Second argument can't be empty" >&2
 else
     re='^[+-]?[0-9]+([.][0-9]+)?$'
     
-    if [[ $arg1 =~ $re ]] && [[ $arg2 =~ $re ]]
+    if [[ $1 =~ $re ]] && [[ $2 =~ $re ]]
     then
         echo -n 'Result: '
-        echo `echo $arg1 + $arg2 | bc`
+        echo `echo $1 + $2 | bc`
 
     else
         echo "Error: Arguments must be numbers" >&2
