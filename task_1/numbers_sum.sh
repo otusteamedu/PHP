d8ca49e8e2effe746e62d438a20f1e7d33123dc6
Alexey Zolotukhin <rustrains@gmail.com>
2019-06-10 10:24:15 +0300

    clear master

--- task_1/numbers_sum.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-re='^[+-]?[0-9]+([.][0-9]+)?$'
-if ! [[ $1 =~ $re ]] || ! [[ $2 =~ $re  ]] ; then
-   echo "error: One or both arguments not a number. You input: $1 and $2" >&1; exit 1
-fi
-
-sum=`echo "$1 + $2" | bc`
-echo "Result: $1 + $2 = $sum."
