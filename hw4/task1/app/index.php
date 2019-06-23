e850b686b75b74e22c54fb826979c9361d3e57c7
Evgeny Prokhorov <prohorov-evgen@ya.ru>
2019-06-23 19:32:41 +0300

    rebuilt task1 with class

--- hw4/task1/app/index.php
+++ hw4/task1/app/index.php
@@ -1,24 +1,8 @@
 <?php
-$response = [
-    'code' => 400,
-    'msg' => 'Something wrong =('
-];
+require_once "vendor/autoload.php";
 
-function checkBrackets(String $str)
-{
-    while (substr_count($str, '()')) {
-        $str = str_replace('()', '', $str);
-    }
-
-    return strlen($str) == 0;
-}
-
-$contentLength = $_SERVER['HTTP_CONTENT_LENGTH'] - strlen('string=');
-
-if (!empty($_POST['string']) && $contentLength == strlen($_POST['string']) && checkBrackets($_POST['string'])) {
-    $response['code'] = 200;
-    $response['msg'] = 'Everything is ok';
-}
+$checker = new Jekys\Brackets('string');
+$response = $checker->getResponse();
 
 http_response_code($response['code']);
 echo $response['msg'];
