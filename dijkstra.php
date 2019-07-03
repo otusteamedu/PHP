3410cba9dd3cdbf3e81012d63b7f0ef2e9be438d
sunlazor <alxpho@gmail.com>
2019-07-04 00:05:56 +0300

    RC1

--- dijkstra.php
+++ dijkstra.php
@@ -1,16 +1,56 @@
 <?php
-
 $weights = [
-    ['1', '2', 1],
-    ['2', '3', 2],
-    ['1', '3', 5],
+['1', '2', 4],
+['2', '3', 15],
+['3', '6', 1],
+['1', '4', 17],
+['2', '5', 6],
+['3', '5', 10],
+['4', '5', 6],
+['4', '6', 12],
+['4', '7', 4],
+['5', '6', 8],
+['6', '9', 9],
+['7', '8', 6],
+['8', '9', 5],
+['8', '10', 11],
+['9', '10', 4],
+['10', '11', 7],
+['10', '12', 2],
+['11', '12', 3],
 ];
 
 $points = [
     '1' => ['distanceToPoint' => PHP_INT_MAX, 'isVisited' => false],
     '2' => ['distanceToPoint' => PHP_INT_MAX, 'isVisited' => false],
     '3' => ['distanceToPoint' => PHP_INT_MAX, 'isVisited' => false],
+    '4' => ['distanceToPoint' => PHP_INT_MAX, 'isVisited' => false],
+    '5' => ['distanceToPoint' => PHP_INT_MAX, 'isVisited' => false],
+    '6' => ['distanceToPoint' => PHP_INT_MAX, 'isVisited' => false],
+    '7' => ['distanceToPoint' => PHP_INT_MAX, 'isVisited' => false],
+    '8' => ['distanceToPoint' => PHP_INT_MAX, 'isVisited' => false],
+    '9' => ['distanceToPoint' => PHP_INT_MAX, 'isVisited' => false],
+    '10' => ['distanceToPoint' => PHP_INT_MAX, 'isVisited' => false],
+    '11' => ['distanceToPoint' => PHP_INT_MAX, 'isVisited' => false],
+    '12' => ['distanceToPoint' => PHP_INT_MAX, 'isVisited' => false],
 ];
+
+//++dbg
+// $weights = [
+//     ['1', '2', 1],
+//     ['2', '3', 2],
+//     ['1', '3', 5],
+//     // ['1', '4', 1],
+//     // ['4', '3', 1],
+// ];
+
+// $points = [
+//     '1' => ['distanceToPoint' => PHP_INT_MAX, 'isVisited' => false],
+//     '2' => ['distanceToPoint' => PHP_INT_MAX, 'isVisited' => false],
+//     '3' => ['distanceToPoint' => PHP_INT_MAX, 'isVisited' => false],
+//     // '4' => ['distanceToPoint' => PHP_INT_MAX, 'isVisited' => false],
+// ];
+//--dbg
 
 function getPointWithNewLength($newLength, string $pointName, array $points) {
     $point = $points[$pointName];
@@ -33,13 +73,66 @@ function getDistanceBetweenNeighbors(string $from, string $to, $weights) {
 
 function doDijkstra(string $from, string $to, array $points, array $weights) {
     $isAllPointsVisited = false;
+    $points[$from]['distanceToPoint'] = 0;
+    // $points[$from]['isVisited'] = true;
+            //++dbg
+        print_r($points);
+            $i = 1;
+            //--dbg
     while(!$isAllPointsVisited) {
         $isAllPointsVisited = true;
-        foreach($points as $point) {
-            if(!$point['isVisited']) {
+        $pointWithTheLeastDistanceTo = ['', PHP_INT_MAX];
+        // Проверям есть ли непосещенные точки и выбираем с наименьшим расстоянием   
+            //++dbg
+            echo 'main cycle start: ' . $i . PHP_EOL;
+            //--dbg
+        foreach($points as $name => $pointData) {      
+            //++dbg
+            echo 'point name: ' . $name . ', ddata: ';
+            var_dump($pointData);
+            echo PHP_EOL;
+        //--dbg
+            if(!$pointData['isVisited']) {
+                if($pointData['distanceToPoint'] < $pointWithTheLeastDistanceTo[1]
+                ) {
+                    $pointWithTheLeastDistanceTo[0] = $name;
+                    $pointWithTheLeastDistanceTo[1] = $pointData['distanceToPoint'];
+                }
+            }
+            
+            //++dbg
+            echo 'Least distance point: ' . $pointWithTheLeastDistanceTo[0] . ', distance: ' . $pointWithTheLeastDistanceTo[1] . PHP_EOL;
+        //--dbg
+        }
+            //++dbg
+            echo 'Selected point: ' . $pointWithTheLeastDistanceTo[0] . ', distance: ' . $pointWithTheLeastDistanceTo[1] . PHP_EOL;
+            //--dbg
+        // Пересчитываем расстояние ближайших точек
+        foreach($points as $name => &$pointData) {
+            // Пометим посещенной минимальную
+            if($name === $pointWithTheLeastDistanceTo[0]) {
+                $pointData['isVisited'] = true;
+                continue;
+            }
+            // Пересчитываем расстояния 
+            if(!$pointData['isVisited']) {
                 $isAllPointsVisited = false;
-
+                $distance = getDistanceBetweenNeighbors($pointWithTheLeastDistanceTo[0], $name, $weights);
+                if($distance) {
+                    $pointData['distanceToPoint'] = $pointWithTheLeastDistanceTo[1] + $distance;
+                }
             }
         }
+        unset($pointData);
+
+        //++dbg
+        print_r($points);
+         echo 'main cycle end: ' . $i . PHP_EOL;
+         $i++;
+        //--dbg
     }
+
+    return 'Distance to point ' . $to . ' is: ' . $points[$to]['distanceToPoint'];
 }
+
+echo doDijkstra('1', '3', $points, $weights);
