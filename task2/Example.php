6600a90ee2e59b95d0ab5e416d6064430b74ea90
Baldin Dmitriy <baldin@tutu.ru>
2019-07-09 18:34:07 +0300

    task1, task2

---
+++ task2/Example.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Dima Baldin <baldin@tutu.ru>
+ *
+ * @description пример использования пакета
+ */
+require_once 'vendor/autoload.php';
+
+use Code\Simple;
+
+var_dump(Simple::isSimple(31));
+var_dump(Simple::isSimple(28));
