cbec29d2ae543a1ef506a189ab7ae00c648f8661
Luft Vitaliy <luft@luft-MacBookPro.office.tutu.ru>
2019-07-16 13:17:40 +0300

    Задание с проверкой почты

---
+++ hw6/MailChecker.class.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+class MailChecker
+{
+    const MAIL_PATTERN = '/^[^@]+@([^@]+)+\.[^@\.]+$/';
+
+    public function checkMail(string $mail): bool
+    {
+        if (
+            $this->checkWithRegularExpression($mail) === true
+            && $this->checkWithDns($mail) === true
+        ) {
+            return true;
+        }
+        return false;
+    }
+
+    private function checkWithRegularExpression(string $mail): bool
+    {
+        return preg_match(self::MAIL_PATTERN, $mail);
+    }
+
+    private function checkWithDns(string $mail): bool
+    {
+        $domain = substr(strrchr($mail, "@"), 1);
+        return getmxrr($domain, $hosts);
+    }
+}
