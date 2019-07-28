8698a1d112d87cb4d3ffcdcf4d6c56e0252f81fa
AnrDaemon <anrdaemon@yandex.ru>
2019-07-28 17:40:38 +0300

    Fixed Dockerfile staging to make more sense
    Fixed copied files ownership inside target container the right way.
    
    Added PHP SAPI name and a list of installed extensions at the bottom.

--- web.php
+++ web.php
@@ -2,3 +2,4 @@
 ob_start();
 phpinfo(~(2+64));
 print str_ireplace('.e {background', '.e {white-space: nowrap; background', ob_get_clean());
+?><ul><li><?=PHP_SAPI?></li><?php array_map(function($name) { print "<li>{$name}</li>"; }, get_loaded_extensions()); ?></ul>
