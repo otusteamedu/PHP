9f8a5d161a3ed6bcde4d6424f30b5b11e71d9297
AnrDaemon <anrdaemon@yandex.ru>
2019-07-29 21:00:06 +0300

    Classes as requested...

---
+++ StreamServer.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace AnrDaemon\Net;
+
+class StreamServer
+{
+  protected $uri;
+  protected $errno;
+  protected $errstr;
+  protected $flags;
+  protected $context;
+
+  protected $connectTimeout;
+  protected $readTimeout;
+
+  protected $server;
+  protected $fallback;
+  protected $handlers = [];
+
+  protected function init()
+  {
+    $params = [
+      $this->uri,
+      &$this->errno,
+      &$this->errstr,
+      $this->flags,
+    ];
+
+    if(is_resource($this->context))
+    {
+      $params[] = $this->context;
+    }
+
+    $this->server = stream_socket_server(...$params);
+    if(false === $this->server)
+    {
+      throw new \RuntimeException("Unable to start server: " . error_get_last()["message"]);
+    }
+  }
+
+  protected function accept($socket)
+  {
+    $client = stream_socket_accept($socket, $this->connectTimeout);
+    if(!$client)
+    {
+      return null;
+    }
+
+    if(false === stream_set_blocking($client, false))
+    {
+      throw new \RuntimeException("Unable to unblock stream!");
+    }
+
+    return $client;
+  }
+
+  protected function dispatch($verb)
+  {
+    return isset($this->handlers["k:$verb"]) ? $this->handlers["k:$verb"] : $this->fallback;
+  }
+
+  public function stop()
+  {
+    if(is_resource($this->server))
+    {
+      if(false === fclose($this->server))
+      {
+        error_log("WTF?");
+      }
+
+      [$shema, $path] = explode(":", $this->uri, 2);
+      if($shema === "unix" && false === unlink($path))
+      {
+        error_log("WTF/2?");
+      }
+    }
+  }
+
+  public function start()
+  {
+    $this->init();
+
+    $rack = [];
+    $buffer = [];
+    while(true)
+    {
+      $read = $rack;
+      $read[] = $this->server;
+
+      stream_select($read, $write, $except, 0, 200000);
+
+      foreach($read as $stream)
+      {
+        if($stream === $this->server)
+        {
+          if($stream = $this->accept($stream, 0))
+          {
+            $rack[] = $stream;
+          }
+          else
+          {
+            error_log("Failed to accept client.");
+            continue;
+          }
+        }
+
+        $id = array_search($stream, $rack, true);
+
+        $s = (isset($buffer[$id]) ? $buffer[$id] : "") . stream_get_contents($stream);
+        while(true)
+        {
+          $delim = strpos($s, "\n");
+          if(false === $delim)
+          {
+            break;
+          }
+
+          [$head, $tail] = explode("\n", $s, 2);
+          $verb = trim($head);
+          if(strlen($verb))
+          {
+            print "$id: $verb\n";
+            $handler = $this->dispatch($verb);
+            $response = $handler($verb);
+            if(false === fwrite($stream, "$response\n"))
+            {
+              error_log("Broken stream $id, client dropped?");
+            }
+          }
+
+          $s = $tail;
+        }
+
+        $buffer[$id] = $s;
+
+        if(feof($stream))
+        {
+          error_log("EOF on stream $id, killing.");
+          fclose($stream);
+          unset($rack[$id], $buffer[$id]);
+          continue;
+        }
+      }
+    }
+  }
+
+  public function setHandler(string $verb, callable $handler)
+  {
+    if(is_resource($this->server))
+    {
+      throw new \BadMethodCallException("Unable to redefine handlers while server is running");
+    }
+
+    if($verb === "DEFAULT")
+    {
+      $this->fallback = $handler;
+    }
+    else
+    {
+      $this->handlers["k:$verb"] = $handler;
+    }
+  }
+
+  public function setFlags(int $flags)
+  {
+    $this->flags = $flags;
+  }
+
+  public function setContext(resource $context)
+  {
+    $this->context = $context;
+  }
+
+  public function setConnectTimeout(int $timeout)
+  {
+    $this->connectTimeout = $timeout;
+  }
+
+  public function setReadTimeout(int $timeout)
+  {
+    $this->readTimeout = $timeout;
+  }
+
+  public function __construct(string $uri, int $flags = STREAM_SERVER_BIND | STREAM_SERVER_LISTEN, resource $context = null)
+  {
+    $this->uri = $uri;
+    $this->flags = $flags;
+    $this->context = $context;
+    $this->connectTimeout = ini_get("default_socket_timeout");
+    $this->readTimeout = ini_get("default_socket_timeout");
+  }
+
+  public function __destruct()
+  {
+    if(is_resource($this->server))
+    {
+      error_log("Shutting down.");
+      $this->stop();
+    }
+  }
+}
--- server.php
+++ server.php
@@ -1,109 +1,22 @@
 #!/usr/bin/env php
 <?php
 
-function s_accept($socket, $timeout = 1)
-{
-  $client = stream_socket_accept($socket, $timeout);
-  if(!$client)
-  {
-    return null;
-  }
+namespace AnrDaemon;
 
-  if(false === stream_set_blocking($client, false))
-  {
-    throw new \RuntimeException("Unable to unblock stream!");
-  }
+require __DIR__ . "/StreamServer.php";
 
-  return $client;
-}
-
-$errno = null;
-$errstr = null;
-$server = stream_socket_server("unix://" . __DIR__ . "/socket", $errno, $errstr);
-//$server = stream_socket_server("tcp://0.0.0.0:4445", $errno, $errstr);
-
-$shutdown = function(...$args)
-use(&$server)
-{
-  error_log("Shutting down.");
-
-  if(false === fclose($server))
-  {
-    error_log("WTF?");
-  }
-
-  if(false === unlink(__DIR__ . "/socket"))
-  {
-    error_log("WTF?");
-  }
-};
-
-register_shutdown_function($shutdown);
+$uri = "unix://" . __DIR__ . "/socket";
+//$uri = "tcp://0.0.0.0:4445";
+$server = new Net\StreamServer($uri);
 
 if(function_exists("pcntl_signal"))
 {
-  pcntl_signal(SIGINT, $shutdown);
+  pcntl_signal(SIGINT, [$server, "stop"]);
 }
 
-$timeout = 0;
-$rack = [];
-$buffer = [];
-while(true)
+$server->setHandler("DEFAULT", function($verb)
 {
-  $read = $rack;
-  $read[] = $server;
+  return "Ok";
+});
 
-  stream_select($read, $write, $except, 0, 200000);
-
-  foreach($read as $stream)
-  {
-    if($stream === $server)
-    {
-      if($stream = s_accept($stream, 0))
-      {
-        $rack[] = $stream;
-      }
-      else
-      {
-        error_log("Failed to accept client.");
-        continue;
-      }
-    }
-
-    $id = array_search($stream, $rack);
-
-    $s = (isset($buffer[$id]) ? $buffer[$id] : "") . stream_get_contents($stream);
-    while(true)
-    {
-      $delim = strpos($s, "\n");
-      if(false === $delim)
-      {
-        $buffer[$id] = $s;
-        break;
-      }
-
-      [$head, $tail] = explode("\n", $s, 2);
-
-      if(strlen($head))
-      {
-        print "$id: $head\n";
-        if(false === fwrite($stream, "Ok\n"))
-        {
-          error_log("Broken stream $id, client dropped?");
-        }
-      }
-
-      $s = $tail;
-    }
-
-    $buffer[$id] = $s;
-
-    if(feof($stream))
-    {
-      error_log("EOF on stream $id, killing.");
-      fclose($stream);
-      unset($rack[$id], $buffer[$id]);
-      continue;
-    }
-  }
-}
+$server->start();
