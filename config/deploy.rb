4597e8e33f035314bd3dc68f24d9c9382348d4f6
f.pronin <f.pronin@fin.media>
2019-08-04 22:12:53 +0300

    Fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,24 @@
 require 'mina/rails'
 require 'mina/git'
 
-set :application_name, 'otus-ci'
+set :application_name, 'otus'
 set :domain, '192.168.1.72'
-set :deploy_to, '/home/serv/otus-ci'
-set :repository, 'git@github.com:fdor/otus-ci.git'
-set :branch, 'master'
+set :deploy_to, '/home/serv/otus'
+set :repository, 'git@github.com:otusteamedu/PHP.git'
+set :branch, 'fdor/hw32-1'
 set :user, 'serv'
 
 task :deploy do
-  deploy do
-    invoke :'git:clone'
-    invoke :'deploy:link_shared_paths'
-    invoke :'deploy:cleanup'
-
-    on :launch do
-      in_path(fetch(:current_path)) do
-        command %{mkdir -p tmp/}
-        command %{touch tmp/restart.txt}
-      end
+    deploy do
+        invoke :'git:clone'
+        invoke :'deploy:link_shared_paths'
+        invoke :'deploy:cleanup'
+        
+        on :launch do
+            in_path(fetch(:current_path)) do
+                command %{mkdir -p tmp/}
+                command %{touch tmp/restart.txt}
+            end
+        end
     end
-  end
 end
-
