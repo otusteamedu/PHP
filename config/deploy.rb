faef5f092751b95b041d2bde4ff0df6332c803be
f.pronin <f.pronin@fin.media>
2019-08-04 22:30:48 +0300

    Fix deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,6 @@ task :deploy do
             in_path(fetch(:current_path)) do
                 command %{mkdir -p tmp/}
                 command %{touch tmp/restart.txt}
-                command %{sudo ./init.sh}
             end
         end
     end
