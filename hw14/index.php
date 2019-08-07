a1673c43958c404fafacf22b36fa29c1b3c3e8dc
Ivan Fomin <la-verne@mail.ru>
2019-08-07 11:18:54 +0300

    Deleted var_dump

--- hw14/index.php
+++ hw14/index.php
@@ -5,8 +5,6 @@ require_once __DIR__ . '/Statistic.php';
 
 $static = new Statistic();
 
-//var_dump($static->addChannel(new YouTubeApi(), 'UCetgtvy93o3i3CvyGXKFU3g'));
-//var_dump($static->deleteChannel('ФАЛТ МФТИ'));
 
 $objects = $static->getViews();
 
