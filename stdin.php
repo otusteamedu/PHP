ab0e6b6bb1e55fc347bfae9c40e7e6a40a1913aa
Semanuilko <B197xT22>
2019-08-19 18:44:37 +0300

    Работа с CLI ООП + настройки

--- stdin.php
+++ stdin.php
@@ -1,22 +1,42 @@
 <?php
-$handle = fopen('php://stdin', 'r');
+class Checker
+{
+    static public $settings = "setting.json";
 
-$commands = array('git commit', 'ps aux', 'nano');
+    static function execute()
+    {
+        $handle = fopen('php://stdin', 'r');
+        while (true) {
+            $buffer = fgets($handle);
+            foreach (self::getCommands() as $value) {
+                $originalWords = substr_count($value, ' ');
+                $commandParts = explode(' ', $buffer);
+                $output = array_slice($commandParts, 0, $originalWords + 1);
+                $sim = similar_text($value, implode(" ", $output), $perc);
 
-while (true) {
-    $buffer = fgets($handle);
-    $sim = similar_text('git commit', $buffer, $perc);
+                if ($perc >= self::getPercent() and $perc < 95) {
+                    echo "F#cking mismatch. You mean : " . $value . " " . implode(" ", array_slice($commandParts, $originalWords + 1)) . PHP_EOL;
+                    exit;
+                }
 
-    foreach ($commands as $value) {
-        $originalWords = substr_count($value, ' ');
-        $commandParts = explode(' ', $buffer);
-        $output = array_slice($commandParts, 0, $originalWords + 1);
-        $sim = similar_text($value, implode(" ", $output), $perc);
-        if ($perc >= 70) {
-            echo "F#cking mismatch. You mean : " . $value . " " . implode(" ", array_slice($commandParts, $originalWords + 1)) . PHP_EOL;
-            exit;
+                if ($value == trim(implode(" ", $output))) {
+                    echo "All good: " . $value . " " . implode(" ", array_slice($commandParts, $originalWords + 1)) . PHP_EOL;
+                    exit;
+                }
+            }
         }
+        fclose($handle);
+    }
+
+    private static function getPercent(){
+        $settings = json_decode(file_get_contents(self::$settings), true);
+        return $settings["perc"];
+    }
+
+    private static function getCommands(){
+        $settings = json_decode(file_get_contents(self::$settings), true);
+        return $settings["commands"];
     }
 }
 
-fclose($handle);
+Checker::execute();
