6fe6b26c66543b77298c65ba7940de3f379135a0
Dmitry Silinsky <sil.dmitrij@gmail.com>
2019-09-12 00:01:36 +0300

    Homework 2 Solution Fix.

--- hw02/sum.sh
+++ hw02/sum.sh
@@ -4,14 +4,14 @@ if ! [ $# -eq 2 ]; then
 	echo "Error: command takes two numbers" >&2
 	exit 1
 else
-	if ! [[ "$1" =~ ^[[:digit:]]+$ ]]; then
+	if ! [[ "$1" =~ ^\-?[[:digit:]]+(\.[[:digit:]]+)?$ ]]; then
 		echo "The first argument must be a number" >&2
 		exit 2
 	fi
-	if ! [[ "$2" =~ ^[[:digit:]]+$ ]]; then
+	if ! [[ "$2" =~ ^\-?[[:digit:]]+([\.][[:digit:]]+)?$ ]]; then
                 echo "The second argument must be a number" >&2
                 exit 3
         fi
 fi
 
-echo "$1 + $2 = $(( $1 + $2 ))"
+printf "%s + %s = %s\n" "$1" "$2" "$( bc <<<"$1 + $2" )"
