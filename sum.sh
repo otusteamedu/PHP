86b1b608fc00aeb0738da326babb7030ff4f6d66
eantonov <antonov.evgenij1987@yandex.ru>
2019-09-11 11:35:54 +0300

    change regexp for negative and float numbers, count arguments

--- sum.sh
+++ sum.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-arg1=$1
-arg2=$2
-re='^[0-9]+$'
+re='^[-]?[0-9]+\.?[0-9]*$'
 
-if [[ $arg1 =~ $re &&  $arg2 =~ $re ]] ; then
-    echo $(($arg1 + $arg2))
-else
+if [[ ! $# -eq 2 ]]; then
+    echo "error: Bad arguments count"
+    exit 1
+elif [[ ! $1 =~ $re ]] || [[ ! $2 =~ $re ]]; then
     echo "error: Bad argument(s). Not a number"
     exit 1
+else
+    echo $1 + $2 |bc
 fi
