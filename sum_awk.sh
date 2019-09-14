ae9fc15711f90e24fa514e9d26b2a01d787db7e5
Andrey Artyushenko <work.a.andrey@gmail.com>
2019-09-14 15:37:32 +0500

    sum_awk: add regexp checks for args

--- sum_awk.sh
+++ sum_awk.sh
@@ -1,3 +1,13 @@
  #!/bin/bash
-
-echo "$1|$2" | awk -F'|' '{sum = $1 + $2} END {print sum}'
+if [ ! $# -eq 2 ]; then
+   echo 'Error: You should pass 2 numbers' >&2
+else
+    for number in $@
+    do
+        if [[ ! $number =~ ^-?[0-9].?[0-9]?+$ ]]; then
+            echo "Error: '$number' is not a number" >&2
+            exit 1
+        fi
+    done
+    echo "$1|$2" | awk -F'|' '{sum = $1 + $2} END {print sum}'
+fi
