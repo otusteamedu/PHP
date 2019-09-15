fbb9554c9cef8f9eaa63746ae34854efe706cc82
omen <omen.ekb@yandex.ru>
2019-09-15 14:03:10 +0500

    add neagativ num and change call num

--- sum.sh
+++ sum.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-read -p "First num: " f_num
-read -p "Second num: " s_num
 
-re='^[0-9]+([.][0-9]+)?$'
-if ! [[  $f_num =~ $re ]] ; then
+re='^([-,0-9])+([.][0-9]+)?$'
+if ! [[  $1 =~ $re ]] ; then
    echo "error: First num not a number" >&2; exit 1
 fi
 
-if ! [[  $s_num =~ $re ]] ; then
+if ! [[  $2 =~ $re ]] ; then
    echo "error: Second num not a number" >&2; exit 1
 fi
 
-echo "$f_num+$s_num" |awk '{split($0,a,"+"); $res=a[1]+a[2]; print $res}'
+echo "$1+$2" |awk '{split($0,a,"+"); $res=a[1]+a[2]; print $res}'
