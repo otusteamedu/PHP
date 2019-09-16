0748319a614d091fe7713a017fde1dce8884aad0
Aleksei <omen.ekb@yandex.ru>
2019-09-16 09:28:23 +0500

    hw2 correct regex

--- sum.sh
+++ sum.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-re='^([-,0-9])+([.][0-9]+)?$'
+re='^(-?[0-9])+([.][0-9]+)?$'
 if ! [[  $1 =~ $re ]] ; then
-   echo "error: First num not a number" >&2; exit 1
+   echo "error: First argument not correct number" >&2; exit 1
 fi
 
 if ! [[  $2 =~ $re ]] ; then
-   echo "error: Second num not a number" >&2; exit 1
+   echo "error: Second argument not correct number" >&2; exit 1
 fi
 
 echo "$1+$2" |awk '{split($0,a,"+"); $res=a[1]+a[2]; print $res}'
