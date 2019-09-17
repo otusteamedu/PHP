7c5b8d86f5600eef3044c0879374621712798658
Katya Leurdo <katya.leurdo@gmail.com>
2019-09-17 10:59:22 +0300

    hw3 p.1 redis install with make

--- textprocess.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-#преобразовываем файл в столбец с городами
-awk -f myscript.awk db.txt
-#сортируем файл по числу вхождений и отрезаем 3 первых записи
-sort cities.txt | uniq -c | sort -nr | head -n 3
-
-
