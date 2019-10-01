6398cd2aab72ca54b32b5208bf46e421f23612ba
justfallingup <just.falling.up@gmail.com>
2019-10-01 14:41:54 +0300

    вещественные числа для суммирования и файл со статистикой по городам как аргумент ввода

--- hw2/hw2_1.sh
+++ hw2/hw2_1.sh
@@ -1,9 +1,10 @@
 #!bin/bash
-number='^-?[0-9]+$'
+number='^-?[0-9]+(\.[0-9]+)?$'
 if [[ $# == 2 ]] && [[ $1 =~ $number ]] && [[ $2 =~ $number ]]
 then
-    sum=$(($1 + $2))
-    echo $sum
+    first=$1
+    second=$2
+    echo $first + $second | bc
 else
-    echo "Error - need two integers"
+    echo "Error - need two numbers"
 fi
--- hw2/hw2_2.sh
+++ hw2/hw2_2.sh
@@ -1,2 +1,8 @@
 #!bin/bash
-awk '{print$3}'| sort | uniq -c | sort -r | awk '{print$2}'| head -n3;
+if [[ $# == 1 ]]
+then
+    inputfile=$1
+    awk '{print$3}' $inputfile | sort | uniq -c | sort -r | awk '{print$2}'| head -n3
+else
+    echo 'Need filename as an argument'
+fi
