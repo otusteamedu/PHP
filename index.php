00e9c035882387ccb8703f5af5ec2ff83e9193b0
Alexey Zolotukhin <rustrains@gmail.com>
2019-10-07 14:23:23 +0300

    hw 25. 1

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?= 'Hello World!!!' ?>
+<?php
+
+$fp = fopen('input.json', 'rb+');
+$json_str = '';
+$arr = [];
+if ($fp) {
+    while(!feof($fp)) {
+        $json_str .= fread($fp, 1024);
+    }
+
+    if (!empty($json_str)) {
+        $arr = json_decode($json_str, true);
+    }
+    fclose($fp);
+}
+
+var_dump($arr);
