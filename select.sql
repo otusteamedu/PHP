a7d6d2f2bd94d5ff20f216a1bcba162efa319165
Andrey <roliol27@yandex.ru>
2019-10-13 05:30:01 +0300

    fix tickets

--- select.sql
+++ select.sql
@@ -1,6 +1,6 @@
-SELECT count(tickets.id_films)*price.price, film.name
+SELECT count(tickets.id)*seance.price as best_selling_movie, film.name as film_name
 FROM public.tickets 
-inner join film on tickets.id_films = film.id 
-inner join price on tickets.id_price = price.id 
-group by tickets.id_films , price.price, film.name
-having count(film.name)=1 order by price desc;
+inner join seance on tickets.id_seance = seance.id 
+inner join film on seance.id_film = film.id 
+group by  seance.price , film.name
+having count(film.name)=1 order by best_selling_movie desc;
