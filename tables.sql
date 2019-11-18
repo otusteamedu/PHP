869d88a7420e3a09ab7dbec3dac7b5bb9613b823
Alexander Korneev <korneev@tutu.ru>
2019-11-18 20:25:06 +0300

    работа над ошибками

--- tables.sql
+++ tables.sql
@@ -1,8 +1,8 @@
 -- создаем таблицы
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY,
-  show_id INT,
-  tickets INT);
+  tickets INT,
+  full_price MONEY);
 
 CREATE TABLE shows (
 id SERIAL PRIMARY KEY,
@@ -22,6 +22,20 @@ CREATE TABLE rooms (
 id SERIAL PRIMARY KEY,
 name VARCHAR(50),
 description VARCHAR(200));
+
+CREATE TABLE tickets (
+id SERIAL PRIMARY KEY,
+order_id INT,
+show_id INT,
+status VARCHAR(10),
+price MONEY,
+tariff_id INT,
+seat_number INT);
+
+CREATE TABLE Tariffs (
+id SERIAL PRIMARY KEY,
+description VARCHAR(200),
+price_factor NUMERIC(1,2));
 
 --наполняем их данными
 INSERT INTO movies (name, description, length)
@@ -49,5 +63,18 @@ VALUES
 ('2019-11-10', '10:00', 2, 2, 800),
 ('2019-11-10', '10:00', 3, 1, 115);
 
-INSERT INTO orders (show_id, tickets)
-VALUES (1, 2), (2, 10), (3, 1), (4, 5), (5, 2), (1, 4);
+INSERT INTO orders (show_id, tickets, full_price)
+VALUES (1, 2, 450.70), (2, 10, 5000), (3, 1, 1500), (4, 5, 900);
+
+INSERT INTO tariffs (description, price_factor)
+VALUES 
+('BASIC', 1.00)
+('VIP', 2.50),
+('Детский тариф - только по выходным', 0.5),
+('Тариф для самых лояльных покупателей', 0.7);
+
+INSERT INTO tickets (order_id, show_id, status, price, tariff_id, seat_number)
+VALUES (1, 2, paid, 500, 3, 23), (2, 2, returned, 333, 1, 23)
+
+
+
