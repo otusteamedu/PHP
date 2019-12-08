6c9738e313127d4f60ff00e9641633d536afdda0
vlyagusha <vlyagusha@justclick.ru>
2019-12-08 18:58:30 +0300

    скрипты для решения заданий 1 и 2

---
+++ task2.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [[ $1 == "" ]]
+then
+    echo "Filename requires"
+    exit 1
+fi
+
+awk -F"[[:space:]]" '$3 != "" { print $3 }' $1 | sort | uniq -c | sort --key=6,7 -r | head -n 1 | awk '{ print $2 }'
+exit 0
