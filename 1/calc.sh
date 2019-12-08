7fa092486f2af7e26421ad2a01d7e61b14375219
VVP <wolodin@vvp24.ru>
2019-12-08 15:45:55 +0500

    add calc.sh

---
+++ 1/calc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+value_1=$1
+value_2=$2
+regexpNumber="^-?[0-9]+(\.[0-9]+)*$"
+
+if [[ -z "$value_1" || -z "$value_2" ]]; then
+  echo "Не переданы необходимые параметры"
+  exit 1
+fi
+
+if [[ ! "$(echo $value_1 | grep -E $regexpNumber)" ]]; then
+  echo "Первый параметр не число"
+  exit 1
+fi
+
+if [[ ! "$(echo $value_2 | grep -E $regexpNumber)" ]]; then
+  echo "Второй параметр не число"
+  exit 1
+fi
+
+sum=$(echo "$value_1 + $value_2" | bc)
+echo "Сумма чисел = $sum"
+
+exit 0
