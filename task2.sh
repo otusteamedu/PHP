b4286b5c0a6eef7c46041e00515e01ecca9f366d
vlyagusha <vlyagusha@justclick.ru>
2019-12-08 23:03:24 +0300

    2 задание - вывод 3 городов

--- task2.sh
+++ task2.sh
@@ -6,5 +6,5 @@ then
     exit 1
 fi
 
-awk -F"[[:space:]]" '$3 != "" { print $3 }' $1 | sort | uniq -c | sort --key=6,7 -r | head -n 1 | awk '{ print $2 }'
+awk -F"[[:space:]]" '$3 != "" { print $3 }' $1 | sort | uniq -c | sort --key=6,7 -r | head -n 3 | awk '{ print $2 }'
 exit 0
