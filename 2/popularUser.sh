72484fc54c9c92a2855d064007d861b5c171149a
VVP <wolodin@vvp24.ru>
2019-12-08 22:48:32 +0500

    work popularUser.sh

--- 2/popularUser.sh
+++ 2/popularUser.sh
@@ -2,9 +2,16 @@
 
 file=$1
 
-if [ -s "$file" ]; then
-    echo "Пуст или несуществует"
+if [[ -z "$file" ]]; then
+    echo "Не передано имя файла"
     exit 1
 fi
+
+if [[ ! -f "$file" || ! -s "$file" ]]; then
+    echo "Файл пуст или несуществует"
+    exit 1
+fi
+
+
 
 exit 0
