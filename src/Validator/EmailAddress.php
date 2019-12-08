ed9901c11c93efdb09fb588956515bba0a6346a1
AnrDaemon <anrdaemon@yandex.ru>
2019-12-09 00:00:20 +0300

    Split different validation steps into their own methods
    Clarified project dependencies.

--- src/Validator/EmailAddress.php
+++ src/Validator/EmailAddress.php
@@ -4,7 +4,7 @@ namespace AnrDaemon\Validator;
 
 class EmailAddress
 {
-  public function validate($address)
+  public function validateSyntax($address)
   {
     $tail = \strrchr($address, "@");
 
@@ -18,12 +18,23 @@ class EmailAddress
       throw new \UnexpectedValueException("No domain part of address in '{$address}'");
     }
 
-    $domain = \idn_to_ascii(\substr($tail, 1), \IDNA_DEFAULT, \INTL_IDNA_VARIANT_UTS46);
+    return true;
+  }
+
+  public function validateDomain($address)
+  {
+    $domain = \idn_to_ascii(\substr(\strrchr($address, "@"), 1), \IDNA_DEFAULT, \INTL_IDNA_VARIANT_UTS46);
+
     if(!(\checkdnsrr($domain, 'MX') || \checkdnsrr($domain, 'A') || \checkdnsrr($domain, 'AAAA')))
     {
       throw new \UnexpectedValueException("No MX/A/AAAA DNS RR found for domain '{$domain}' of address '{$address}'");
     }
 
     return true;
+  }
+
+  public function validate($address)
+  {
+    return $this->validateSyntax($address) && $this->validateDomain($address);
   }
 }
