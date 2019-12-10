a706044e1332084fcdaa8b42d538054dd8f814d7
alexey.mostovoy <alexey.mostovoy@rtlabs.ru>
2019-12-10 11:40:44 +0300

    Не принимает числа числа 1-2-2, 10. , 12.00-5

--- linux_hw_1.sh
+++ linux_hw_1.sh
@@ -3,7 +3,7 @@ if [ $# -ne "2" ]; then
   echo "Order of use: $0 logged_1 logged_2"
   exit 65
 fi
-if [ -z "$(echo $1 | awk '/^[0-9\.\-]*$/{print $0}')" -o -z "$(echo $2 | awk '/^[0-9\.\-]*$/{print $0}')" ]; then
+if [ -z "$(echo $1 | awk '/^[\-]?[0-9]*[\.]?[0-9]+$/{print $0}')" -o -z "$(echo $2 | awk '/^[\-]?[0-9]*[\.]?[0-9]+$/{print $0}')" ]; then
   echo "Order of use: $0 numeric numeric"
   exit 65
 fi
