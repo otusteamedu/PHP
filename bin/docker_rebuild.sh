66d0d4281dd1383ffa90a7ced8e9758c824193a2
vlyagusha <vlyagusha@justclick.ru>
2019-12-13 19:30:15 +0300

    task3 - переместил весь докер в docker/

--- bin/docker_rebuild.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-docker-compose down --remove-orphans
-docker volume prune -f
-docker-compose build
--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker-compose up -d
+docker-compose -f docker/docker-compose.yml up -d
 docker exec -it vlyagusha-app /bin/bash
