1042cbc37901d026b1b8d7fb2a8595e2f9bd1b84
Evgeniy Baranov <evrimedont@gmail.com>
2019-12-25 00:41:28 +0300

    Вторая часть hw3

--- public/index.php
+++ public/index.php
@@ -1,4 +1,11 @@
 <?php
 
-phpinfo();
+use Evrimedont\CalculatorService;
 
+require dirname(__DIR__).'/vendor/autoload.php';
+
+$calculator = new CalculatorService();
+
+$sum = $calculator->addition(10, 20);
+
+echo $sum;
