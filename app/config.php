057d6d387053834212e7d6e79df71364c1201bd7
Azat Nizameev <azat.nizam@gmail.com>
2019-12-25 11:33:29 +0300

    Add config.ini

--- app/config.php
+++ app/config.php
@@ -2,15 +2,27 @@
 namespace Otus\Azatnizam;
 
 class Config {
-    const UNIX_SOCKET_FILE = 'chat.socket';
-
-    const UNIX_RESPONSE_SOCKET_FILE = 'response.chat.socket';
+    const CONFIG_FILE = 'config.ini';
 
     static function getSocketName() {
-        return self::UNIX_SOCKET_FILE;
+        return self::_getValue('unix_socket_file');
     }
 
     static function getResponseSocketName() {
-        return self::UNIX_RESPONSE_SOCKET_FILE;
+        return self::_getValue('unix_response_socket_file');
+    }
+
+    /**
+     * @param string $configField
+     * @return bool|mixed
+     */
+    protected static function _getValue(string $configField) {
+        $arIni = parse_ini_file(self::CONFIG_FILE);
+
+        if ($arIni[$configField] == null) {
+            return false;
+        }
+
+        return $arIni[$configField];
     }
 }
