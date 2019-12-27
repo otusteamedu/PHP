d53bbb0e5081eb38e5456eaa32f8c2052cab99fe
Pavel Sheglov <sheglov.p@brinex.ru>
2019-12-27 09:03:25 +0300

    hw04/task01 избавился от die() hw04/task02 добавил composer install при запуске контейнера

--- hw04/task01/app/public_html/App/Response.php
+++ hw04/task01/app/public_html/App/Response.php
@@ -8,13 +8,11 @@ class Response
     {
         header("HTTP/1.1 200 OK");
         echo $message;
-        die();
     }
 
     public static function sendFail(string $message = '')
     {
         header("HTTP/1.0 400 Bad Reques");
         echo $message;
-        die();
     }
 }
