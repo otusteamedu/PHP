6facecf92727d6469966544120398d281ede155b
VVP <wolodin@vvp24.ru>
2019-12-31 21:29:03 +0500

    add web-entrypoint.sh

--- 3/tools/web-entrypoint.sh
+++ 3/tools/web-entrypoint.sh
@@ -18,7 +18,7 @@ fi
 while IFS= read -r filePath ; do
     [[ -n "$filePath" ]] || continue
     sed -i -e "s@%RUN_DIR%@${DIR_RUN_ENV}@g" "$filePath"
-    sed -i -e "s@%PROJECTS_USERNAME%@${PROJECTS_USERNAME_ENV}@g" "$filePath"
+    sed -i -e "s@%PROJECTS_USERNAME%@${WEB_USERNAME_ENV}@g" "$filePath"
     sed -i -e "s@%MAPPING_PORT_80%@${UNPRIVILEGUE_PORT_MAPPING_80_ENV}@g" "$filePath"
     sed -i -e "s@%MAPPING_PORT_443%@${UNPRIVILEGUE_PORT_MAPPING_443_ENV}@g" "$filePath"
 
@@ -27,7 +27,7 @@ while IFS= read -r filePath ; do
 #    sed -i "s|/var/log/php|${PHP_LOGS_DIR}|g" "$filePath"
 #    sed -i -r "s|^(\s*)listen\s+80\s*|\1listen $NGINX_LISTEN_PORT |g" "$filePath"
 #    sed -i -r "s|^(\s*)listen\s+\[::\]:80\s*|\1listen [::]:$NGINX_LISTEN_PORT |g" "$filePath"
-done <<< "$(find /etc/nginx/sites-includes -type f -name '*.conf' && find /etc/nginx/sites-enabled -type f -name '*.conf')"
+done <<< "$(find /etc/nginx/sites-includes -type f -name '*.conf' || find /etc/nginx/sites-enabled -type f -name '*.conf')"
 
 echo "Setting permissions for the docker container..."
 /tools/permission_fix.sh || true
