e750e8bd1ebb6c42f6b11615c4664397126f19d5
VVP <wolodin@vvp24.ru>
2020-01-02 00:12:33 +0500

    fix docker-compose.yml, web-entrypoint.sh

--- 3/tools/web-entrypoint.sh
+++ 3/tools/web-entrypoint.sh
@@ -7,18 +7,18 @@ set -euo pipefail
 : "${BACKEND_HOST_PORT:=9000}"
 
 if [[ -d /etc/nginx/sites-available.ro ]] ; then
-    rm -rf /etc/nginx/sites-available
-    cp -r /etc/nginx/sites-available.ro /etc/nginx/sites-available
+    rm -rf /etc/nginx/sites-available/*.conf
+    cp -r /etc/nginx/sites-available.ro/*.conf /etc/nginx/sites-available
 fi
 if [[ -d /etc/nginx/sites-enabled.ro ]] ; then
-    rm -rf /etc/nginx/sites-enabled
-    cp -r /etc/nginx/sites-enabled.ro /etc/nginx/sites-enabled
+    rm -rf /etc/nginx/sites-enabled/*.conf
+    cp -r /etc/nginx/sites-enabled.ro/*.conf /etc/nginx/sites-enabled
 fi
 
 while IFS= read -r filePath ; do
     [[ -n "$filePath" ]] || continue
     sed -i -e "s@%RUN_DIR%@${DIR_RUN_ENV}@g" "$filePath"
-    sed -i -e "s@%PROJECTS_USERNAME%@${WEB_USERNAME_ENV}@g" "$filePath"
+    sed -i -e "s@%USERNAME%@${WEB_USERNAME_ENV}@g" "$filePath"
     sed -i -e "s@%MAPPING_PORT_80%@${UNPRIVILEGUE_PORT_MAPPING_80_ENV}@g" "$filePath"
     sed -i -e "s@%MAPPING_PORT_443%@${UNPRIVILEGUE_PORT_MAPPING_443_ENV}@g" "$filePath"
 
@@ -27,11 +27,12 @@ while IFS= read -r filePath ; do
 #    sed -i "s|/var/log/php|${PHP_LOGS_DIR}|g" "$filePath"
 #    sed -i -r "s|^(\s*)listen\s+80\s*|\1listen $NGINX_LISTEN_PORT |g" "$filePath"
 #    sed -i -r "s|^(\s*)listen\s+\[::\]:80\s*|\1listen [::]:$NGINX_LISTEN_PORT |g" "$filePath"
-done <<< "$(find /etc/nginx/sites-includes -type f -name '*.conf' || find /etc/nginx/sites-enabled -type f -name '*.conf')"
+done <<< "$(find /etc/nginx/sites-available -type f -name '*.conf' && find /etc/nginx/sites-enabled -type f -name '*.conf')"
 
-echo "Setting permissions for the docker container..."
-/tools/permission_fix.sh || true
-chown -R ${DOCKER_USER}:${DOCKER_GROUP} ${VOLUME} || true
-echo "Done."
+#echo "Setting permissions for the docker container..."
+#/tools/permission_fix.sh || true
+#chown -R ${DOCKER_USER}:${DOCKER_GROUP} ${VOLUME} || true
+#echo "Done."
+#exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
 
 exec "$@"
