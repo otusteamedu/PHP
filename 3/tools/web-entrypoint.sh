89f18d96b0d1be83d2507670a2454f503e53599c
VVP <wolodin@vvp24.ru>
2020-01-02 00:27:20 +0500

    fix docker-compose.yml, web-entrypoint.sh

--- 3/tools/web-entrypoint.sh
+++ 3/tools/web-entrypoint.sh
@@ -2,8 +2,9 @@
 
 set -euo pipefail
 
-: "${DC_USER_ID:=}"
-: "${DC_GROUP_ID:=}"
+: "${HOST_USER_ID:=1000}"
+: "${HOST_GROUP_ID:=1000}"
+: "${WEB_USERNAME_ENV:=web}"
 : "${BACKEND_HOST_PORT:=9000}"
 
 if [[ -d /etc/nginx/sites-available.ro ]] ; then
@@ -21,18 +22,12 @@ while IFS= read -r filePath ; do
     sed -i -e "s@%USERNAME%@${WEB_USERNAME_ENV}@g" "$filePath"
     sed -i -e "s@%MAPPING_PORT_80%@${UNPRIVILEGUE_PORT_MAPPING_80_ENV}@g" "$filePath"
     sed -i -e "s@%MAPPING_PORT_443%@${UNPRIVILEGUE_PORT_MAPPING_443_ENV}@g" "$filePath"
-
-#    sed -i "s|backend:9000|${BACKEND_HOST_NAME}:${BACKEND_HOST_PORT}|g" "$filePath"
-#    sed -i "s|/var/log/nginx|${NGINX_LOGS_DIR}|g" "$filePath"
-#    sed -i "s|/var/log/php|${PHP_LOGS_DIR}|g" "$filePath"
-#    sed -i -r "s|^(\s*)listen\s+80\s*|\1listen $NGINX_LISTEN_PORT |g" "$filePath"
-#    sed -i -r "s|^(\s*)listen\s+\[::\]:80\s*|\1listen [::]:$NGINX_LISTEN_PORT |g" "$filePath"
 done <<< "$(find /etc/nginx/sites-available -type f -name '*.conf' && find /etc/nginx/sites-enabled -type f -name '*.conf')"
 
-#echo "Setting permissions for the docker container..."
-#/tools/permission_fix.sh || true
-#chown -R ${DOCKER_USER}:${DOCKER_GROUP} ${VOLUME} || true
-#echo "Done."
+echo "Setting permissions for the docker container..."
+/tools/permission_fix.sh || true
+chown -R ${WEB_USERNAME_ENV}:${WEB_USERNAME_ENV} ${VOLUME} || true
+echo "Done."
 #exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
 
 exec "$@"
