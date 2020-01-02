b804e19456a5c36554248ab83fb563f7c0faef8d
VVP <wolodin@vvp24.ru>
2020-01-02 18:14:31 +0500

    fix docker-compose.yml, web-entrypoint.sh

--- 3/tools/web-entrypoint.sh
+++ 3/tools/web-entrypoint.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 : "${HOST_USER_ID:=1000}"
 : "${HOST_GROUP_ID:=1000}"
-: "${WEB_USERNAME_ENV:=web}"
+: "${DOCKER_USERNAME_ENV:=web}"
 : "${BACKEND_HOST_PORT:=9000}"
 
 if [[ -d /etc/nginx/sites-available.ro ]] ; then
@@ -19,15 +19,14 @@ fi
 while IFS= read -r filePath ; do
     [[ -n "$filePath" ]] || continue
     sed -i -e "s@%RUN_DIR%@${DIR_RUN_ENV}@g" "$filePath"
-    sed -i -e "s@%USERNAME%@${WEB_USERNAME_ENV}@g" "$filePath"
+    sed -i -e "s@%USERNAME%@${DOCKER_USERNAME_ENV}@g" "$filePath"
     sed -i -e "s@%MAPPING_PORT_80%@${UNPRIVILEGUE_PORT_MAPPING_80_ENV}@g" "$filePath"
     sed -i -e "s@%MAPPING_PORT_443%@${UNPRIVILEGUE_PORT_MAPPING_443_ENV}@g" "$filePath"
 done <<< "$(find /etc/nginx/sites-available -type f -name '*.conf' && find /etc/nginx/sites-enabled -type f -name '*.conf')"
 
 echo "Setting permissions for the docker container..."
 /tools/permission_fix.sh || true
-chown -R ${WEB_USERNAME_ENV}:${WEB_USERNAME_ENV} ${VOLUME} || true
+chown -R ${DOCKER_USERNAME_ENV}:${DOCKER_USERNAME_ENV} ${VOLUME} || true
 echo "Done."
-#exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
 
 exec "$@"
