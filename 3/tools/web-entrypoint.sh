81dba551b4f9801da666b8a942056d8261623bd6
VVP <wolodin@vvp24.ru>
2020-01-03 18:48:13 +0500

    fix web-entrypoint.sh

--- 3/tools/web-entrypoint.sh
+++ 3/tools/web-entrypoint.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 
-set -eu
-#o pipefail
+set -euo pipefail
 
-: "${HOST_USER_ID:=1000}"
-: "${HOST_GROUP_ID:=1000}"
 : "${DOCKER_USERNAME_ENV:=web}"
 : "${DOCKER_WORKDIR_ENV:=/home/web}"
 : "${BACKEND_HOST_PORT:=9000}"
@@ -29,46 +26,5 @@ while IFS= read -r filePath ; do
     sed -i -e "s@%MAPPING_PORT_443%@${UNPRIVILEGUE_PORT_MAPPING_443_ENV}@g" "$filePath"
 done <<< "$(find /etc/nginx/sites-available -type f -name '*.conf' && find /etc/nginx/sites-enabled -type f -name '*.conf')"
 
-
-echo "Setting permissions for the docker container..."
-
-UNUSED_USER_ID=21338
-UNUSED_GROUP_ID=21337
-
-# Setting Group Permissions
-DOCKER_GROUP_CURRENT_ID=`id -g ${DOCKER_USERNAME_ENV}`
-if [[ ${DOCKER_GROUP_CURRENT_ID} -ne ${HOST_GROUP_ID} ]]; then
-    echo "Check if group with ID ${HOST_GROUP_ID} already exists"
-    DOCKER_GROUP_OLD=`getent group ${HOST_GROUP_ID} | cut -d: -f1`
-
-    if [[ -n ${DOCKER_GROUP_OLD} ]]; then
-        echo "Changing the ID of ${DOCKER_GROUP_OLD} group to 21337"
-        groupmod -o -g ${UNUSED_GROUP_ID} ${DOCKER_GROUP_OLD}
-    fi
-
-    echo "Changing the ID of ${DOCKER_USERNAME_ENV} group to ${HOST_GROUP_ID}"
-    groupmod -o -g ${HOST_GROUP_ID} ${DOCKER_USERNAME_ENV} || true
-    echo "Finished"
-fi
-
-# Setting User Permissions
-DOCKER_USER_CURRENT_ID=`id -u ${DOCKER_USERNAME_ENV}`
-if [[ ${DOCKER_USER_CURRENT_ID} -ne ${HOST_USER_ID} ]]; then
-    echo "Check if user with ID $HOST_USER_ID already exists"
-    DOCKER_USER_OLD=`getent passwd ${HOST_USER_ID} | cut -d: -f1`
-
-    if [[ -n ${DOCKER_USER_OLD} ]]; then
-        echo "Changing the ID of ${DOCKER_USER_OLD} to 21337"
-        usermod -o -u ${UNUSED_USER_ID} ${DOCKER_USER_OLD}
-    fi
-
-    echo "Changing the ID of ${DOCKER_USERNAME_ENV} user to ${HOST_USER_ID}"
-    usermod -o -u ${HOST_USER_ID} ${DOCKER_USERNAME_ENV} || true
-    echo "Finished"
-fi
-
-chown -R ${DOCKER_USERNAME_ENV}:${DOCKER_USERNAME_ENV} ${DOCKER_WORKDIR_ENV} || true
-echo "Done."
-
 # start
-exec "$@"
+exec nginx -g 'daemon off;'
