128f50bc7e2a26e6158143e9e6f0b6879762461f
rudinandrey <rudinandrey@yandex.ru>
2020-01-06 02:30:57 +0300

    hw6 nginx balanser and email validator MVP

--- site/index.php
+++ site/index.php
@@ -1,26 +1,35 @@
 <?php
 
-$string = $_POST["string"];
+$server_ip =  $_SERVER['SERVER_ADDR'];
 
-$arr = str_split($string);
-$status = 0;
+$emails = $_POST["emails"];
 
-try {
-    if(trim($string) == "") throw new Exception("Пустая переменная string");
-    foreach($arr as $a) {
-        if($a == "(") $status += 1;
-        if($a == ")") $status -= 1;
-        if($status < 0) throw new Exception("Неправильный порядок скобок");
+$arr = explode("\n", $emails);
+
+function check_email_reg($email) {
+    $emailB = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+    if (filter_var($emailB, FILTER_VALIDATE_EMAIL) === false ||
+        $emailB != $email
+    ) {
+        return false;
     }
-
-    if($status != 0) throw new Exception("Количество открытых и закрытых скобок различно");
-
-    header("HTTP/1.0 200 OK");
-    print("Все просто отлично");
-} catch (Exception $e) {
-    http_response_code(400);
-    print($e->getMessage());
+    return true;
 }
 
+function check_email_mx($email) {
+    list($user, $domain) = explode('@', $email);
+    $arr= dns_get_record($domain,DNS_MX);
+    if($arr[0]['host']==$domain&&!empty($arr[0]['target'])){
+        return $arr[0]['target'];
+    }
+}
+echo "Server IP : ".$server_ip."<br />";
+echo "<pre>";
+foreach($arr as $e) {
+    if(trim($e) != "") {
+        echo $e."\t".check_email_reg($e)."\t".check_email_mx($e)."\n";
+    }
+}
 
-
+echo "<pre>";
