6cb1f0a928446543fb0a25112c9d59c024bed220
Ilya <whitecomp777@yandex.ru>
2020-01-07 19:07:06 +0300

    Adds first zero handling

--- hw2/task1/script.sh
+++ hw2/task1/script.sh
@@ -32,6 +32,17 @@ fi
 first_fraction_length=${#first_fraction}
 second_fraction_length=${#second_fraction}
 max_fraction_length=0
+
+#removes first zeros
+while [[ ${first_full:0:1} == "0" ]]
+do
+    first_full="${first_full:1}"
+done
+while [[ ${second_full:0:1} == "0" ]]
+do
+    second_full="${second_full:1}"
+done
+
 first="${first_full}${first_fraction}"
 second="${second_full}${second_fraction}"
 
@@ -48,12 +59,16 @@ if [[ ${first_fraction_length} -gt ${second_fraction_length} ]]; then
     done
 fi
 
-result=$(( first + second))
+result=$(( first + second ))
 
 result_length=${#result}
 dot_position=$((result_length - max_fraction_length))
 for ((i = 0; i < $result_length; i++)); do
     if [[ ${dot_position} == ${i} ]]; then
+    #if dot is first character adds zero
+    if [[ ${i} == 0 ]]; then
+        echo -ne "0"
+    fi
     echo -ne "."
     fi
     echo -ne "${result:$i:1}"
