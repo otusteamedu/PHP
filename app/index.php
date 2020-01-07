da85c58c45472d5a2fbb718e0b5c2626a9941a64
Azat Nizameev <azat.nizam@gmail.com>
2020-01-07 16:45:21 +0300

    hw5, task #1 done

--- app/index.php
+++ app/index.php
@@ -1,2 +1,1 @@
-<?php
-phpinfo();
+<a href="task1.php">Task #1</a>&nbsp;<a href="task2.php">Task #2</a>
---
+++ app/task2.php
@@ -0,0 +1,3 @@
+<?php
+header('HTTP/1.1 400 Bad Request');
+echo 'Use git branch <i>azatnizam/hw5/2</i>';
