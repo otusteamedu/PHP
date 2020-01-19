8f63655fdf3ed84a2a6bbc5ef4fdbf2f44d4002d
tirei01 <minicode@yandex.ru>
2020-01-19 22:43:42 +0300

    Сохранил работу

---
+++ www/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Testing</title>
+</head>
+<body>
+<h1>Hellow World</h1>
+<?php // TODO DEL THIS
+echo "<pre style='color:red; clear: both;'>";
+print_r($_SERVER['SERVER_ADDR']);
+echo "</pre>"; ?>
+</body>
+</html>
