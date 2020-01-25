b56e6dcd3da07c94688780519035847e06e9832b
alexander <alexandervasiliev23@gmail.com>
2020-01-25 17:59:56 +0300

    Создание сервера и клиента для передачи сообщений с помощью unix сокетов

--- server.php
+++ server.php
@@ -4,10 +4,7 @@
 use App\Socket\SocketServer;
 
 require_once 'vendor/autoload.php';
-
 $config = require 'config.php';
 
 $serverSocket = new SocketServer($config['socket'] ?? []);
 $serverSocket->serve();
-
-echo 'Hello!'.PHP_EOL;
