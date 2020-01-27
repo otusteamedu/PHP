c7dad0c216ee9542ad74117a42d201253da92674
rudinandrey <rudinandrey@yandex.ru>
2020-01-27 23:36:56 +0300

    Попытка исправить конфликтную ситуацию

--- ddl.sql
+++ ddl.sql
@@ -47,8 +47,8 @@ CREATE TABLE public.ticket (
 -- самый прибыльный фильм
 select movie.movie_name, sum("session".price) as sum from ticket
 left join "session" ON "session".session_id = ticket.session_id
-left join movie on movie.movie_id = "session".movie_id group by "movie".movie_id;
-
+left join movie on movie.movie_id = "session".movie_id group by "movie".movie_id; --  ----- 
+--  -----
 
 CREATE TYPE movie_attributes AS ENUM (
 	'string',
