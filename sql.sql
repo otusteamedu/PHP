1ba0635c39b2be2d5c6259818176dc659576caa8
tirei01 <minicode@yandex.ru>
2020-01-29 00:32:07 +0300

    Добавил связи

--- sql.sql
+++ sql.sql
@@ -1,2 +1,18 @@
 ALTER TABLE hw7.films.ticket
     ADD FOREIGN KEY ("film_id") REFERENCES hw7.films.films ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE hw7.films.ticket
+    ADD FOREIGN KEY ("hall_id") REFERENCES hw7.films.hall ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE hw7.films.ticket
+    ADD FOREIGN KEY ("sessions_id") REFERENCES hw7.films.sessions ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE hw7.films.ticket
+    ADD FOREIGN KEY ("places_id") REFERENCES hw7.films.places ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+ALTER TABLE hw7.films.sessions
+    ADD FOREIGN KEY ("film_id") REFERENCES hw7.films.films ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+
+ALTER TABLE hw7.films.places
+    ADD FOREIGN KEY ("hall_id") REFERENCES hw7.films.hall ("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
