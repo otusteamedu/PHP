5bc72478d5a06f171ee42584611118ee83697da5
vasilyev <vasilyev@ugol.me>
2020-02-04 12:56:23 +0300

    1. Добавил файл .editorconfig в gitignore 2. Вынес логику обработки запроса из фронт-контроллера (index.php) в отдельный класс App 3. Добавил папку vendor в gitignore

--- parentheses-validator/app/index.php
+++ parentheses-validator/app/index.php
@@ -1,36 +1,8 @@
 <?php
 
-use App\ParenthesesValidator;
+use App\App;
 
 require_once 'vendor/autoload.php';
 
-try {
-    $method = $_SERVER['REQUEST_METHOD'] ?? null;
-    $string = $_POST['string'] ?? null;
-
-    if ($method !== 'POST') {
-        throw new BadMethodCallException('Доступен только POST запрос');
-    }
-
-    if ($string === null) {
-        throw new InvalidArgumentException('Параметр "string" обязателен');
-    }
-
-    if ($string === '') {
-        throw new InvalidArgumentException('Параметр "string" не может быть пустым');
-    }
-
-    $parenthesesValidator = new ParenthesesValidator();
-    $isValidString        = $parenthesesValidator->validate($string);
-
-    if ($isValidString) {
-        echo 'Данные успешно прошли валидацию';
-
-        return;
-    }
-
-    throw new InvalidArgumentException('Данные не прошли валидацию');
-} catch (Throwable $exception) {
-    http_response_code(400);
-    echo $exception->getMessage();
-}
+$app = new App();
+$app->run();
