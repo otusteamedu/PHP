10260061be0b62320915aa5ee15757e7bc31bfd5
VVP <wolodin@vvp24.ru>
2020-02-06 12:59:56 +0500

    add vlucas/phpdotenv and bootstrap

--- code/receive.php
+++ code/receive.php
@@ -1,5 +1,9 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/bootstrap.php';
+
+$factory = new \Socket\Raw\Factory();
+// create stream connection socket server
+$socket = $factory->createServer(getenv('SOCKET_PATH') ?: 'unix:///socks/server.sock');
 
 while (true){
     echo 123;
--- code/send.php
+++ code/send.php
@@ -1,7 +1,12 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/bootstrap.php';
 
-while (true){
+$factory = new \Socket\Raw\Factory();
+// create stream connection socket server
+$socket = $factory->createClient(getenv('SOCKET_PATH') ?: 'unix:///socks/server.sock');
+
+while (true) {
+    
     echo 123;
     sleep(10);
 }
