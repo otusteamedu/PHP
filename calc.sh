9fdc92b59ad3014d11558da248b4e9808654a1fb
Nikita <Ozy@nxt.ru>
2020-02-13 09:13:04 +0300

    Числа с плавающей запятой

--- calc.sh
+++ calc.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-reg="^[0-9]+$"
+reg="^([0-9]+(\.[0-9]+)?)$"
 if [[ ! $1 =~ $reg ]] || [[ ! $2 =~ $reg ]]; then
 echo "invalid parameters"
 else
-echo $(( $1 + $2 ))
+echo $1 + $2 | bc
 fi
 exit 0
