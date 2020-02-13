a55647a668bcbece73efc5e83092bda8bba388de
Nikita <Ozy@nxt.ru>
2020-02-13 11:15:14 +0300

    числовая сортировка

--- users.sh
+++ users.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-awk -F' ' '/^[0-9]/{print $3}' ./users.txt | sort | uniq -c | sort -r | head -n3
+awk -F' ' '/^[0-9]/{print $3}' ./users.txt | sort | uniq -c | sort -nr | head -n3
 exit 0
