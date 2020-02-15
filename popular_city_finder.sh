28fa6445e47eca09b7344c74fa8508b9aee8a0cc
Sergey <dante.fxx@gmail.com>
2020-02-15 21:29:50 +0300

    add a restriction for cities count

--- popular_city_finder.sh
+++ popular_city_finder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-tail -n+2 database.txt | awk -F" " '{ print $3 }' | sort | uniq -c | sed 's/^\s*//' | sort -t " " -k1,1nr -k2,2
+tail -n+2 database.txt | awk -F" " '{ print $3 }' | sort | uniq -c | sed 's/^\s*//' | sort -t " " -k1,1nr -k2,2 | head -n3
 
 exit 0
