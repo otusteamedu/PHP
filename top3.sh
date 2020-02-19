971673b0c3d958be2adb3e249ae47592c9d4bf4d
s.ksenofontov <ksenofontovsm@yandex.ru>
2020-02-19 23:28:28 +0500

    Домашнее задание 2. 1. Изменение калькулятора для работы с вещественными числами. 2. Сортировка городов с 1 выховом sort.

--- top3.sh
+++ top3.sh
@@ -5,5 +5,5 @@ then
     echo "Введите имя файла"
     exit 1
 fi
-awk '($3 != "") && (NR > 1) { print $3 }' $1 | sort | uniq -c | sort -k1 -r | head -n3 | awk '{ print $2 }'
+awk '($3 != "") && (NR > 1)' $1 | awk '++array[$3] { print array[$3] " " $3  }' | sort -r | awk '!array[$2]++' | head -n3 | awk '{ print $2 }'
 exit 0
