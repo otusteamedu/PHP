1d2c0d71e473af9d72f6fa2b820eddd1d8154248
VVP <wolodin@vvp24.ru>
2020-02-20 12:24:00 +0500

    add docker-compose.yml config

--- tools/web-entrypoint.sh
+++ tools/web-entrypoint.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 : "${DOCKER_USERNAME_ENV:=web}"
 : "${UNPRIVILEGUE_PORT_MAPPING_80_ENV:=8080}"
 : "${UNPRIVILEGUE_PORT_MAPPING_443_ENV:=8443}"
+: "${UNIX_PHP_SOCK:=unix:/socks/php7.3-fpm-1.sock}"
 
 # Copy nginx config
 if [[ -d /etc/nginx/sites-available.ro ]] ; then
@@ -22,6 +23,7 @@ while IFS= read -r filePath ; do
     sed -i -e "s@%USERNAME%@${DOCKER_USERNAME_ENV}@g" "$filePath"
     sed -i -e "s@%MAPPING_PORT_80%@${UNPRIVILEGUE_PORT_MAPPING_80_ENV}@g" "$filePath"
     sed -i -e "s@%MAPPING_PORT_443%@${UNPRIVILEGUE_PORT_MAPPING_443_ENV}@g" "$filePath"
+    sed -i -e "s@%UNIX_PHP_SOCK%@${UNIX_PHP_SOCK}@g" "$filePath"
 done <<< "$(find /etc/nginx/sites-available -type f -name '*.conf' && find /etc/nginx/sites-enabled -type f -name '*.conf')"
 
 # start
