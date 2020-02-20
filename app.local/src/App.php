a2048c92626b35f852f2b9e2dc11ca30805a5575
VVP <wolodin@vvp24.ru>
2020-02-20 12:38:59 +0500

    add show ip

--- app.local/src/App.php
+++ app.local/src/App.php
@@ -19,26 +19,27 @@ class App
     public function exec(Request $request): ResponseSuccess
     {
         //Проверяем что метод POST и авторизацию
-        if ($request->isMethod(Request::METHOD_POST)) {
-            $data = $request->get('string');
-            if (empty($data)) {
-                throw new RequestException('Error input data: empty string');
-            }
-            if (!is_string($data)) {
-                throw new RequestException('Error input data: not string');
-            }
-            
-            $checkBrackets = new \Library\CheckBrackets();
-            if (!$checkBrackets->isValid($data)) {
-                throw new RequestException('Error input data: incorrect string');
-            }
-            
-            $result         = new ResponseSuccess();
-            $result->status = 'ok';
-            $result->result = "String: {$data} is valid";
-            
-            return $result;
-        }
+//        if ($request->isMethod(Request::METHOD_POST)) {
+//            $data = $request->get('string');
+//            if (empty($data)) {
+//                throw new RequestException('Error input data: empty string');
+//            }
+//            if (!is_string($data)) {
+//                throw new RequestException('Error input data: not string');
+//            }
+//
+//            $checkBrackets = new \Library\CheckBrackets();
+//            if (!$checkBrackets->isValid($data)) {
+//                throw new RequestException('Error input data: incorrect string');
+//            }
+//
+//            $result         = new ResponseSuccess();
+//            $result->status = 'ok';
+//            $result->result = "String: {$data} is valid";
+//
+//            return $result;
+//        }
+        return $_SERVER['REMOTE_ADDR'];
         
         throw new RequestException('Not POST request');
     }
