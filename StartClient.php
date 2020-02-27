15a3f48f9ca5de58d469575c3e5d4720a3be8def
Astrakhantsev Viktor <astrviktor@gmail.com>
2020-02-27 20:17:48 +0300

    homework 4

---
+++ StartClient.php
@@ -0,0 +1,6 @@
+<?php
+
+include_once "src/SockClient.php";
+
+$Client = new SockClient();
+$Client->Run();
---
+++ StartServer.php
@@ -0,0 +1,6 @@
+<?php
+
+include_once "src/SockServer.php";
+
+$Server = new SockServer();
+$Server->Run();
