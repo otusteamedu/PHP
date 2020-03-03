25f4a374a0e4cffaa2896792835514402158ee86
Vlad Duplin <vlad.duplin@ya.ru>
2020-03-03 22:12:49 +0300

    HW6-1 Валидация POST запроса

--- src/Main.php
+++ src/Main.php
@@ -2,12 +2,29 @@
 
 namespace App;
 
-use Bjlag\PhpInfo;
+use App\Exceptions\BadRequestException;
+use App\Exceptions\MethodNotAllowedException;
+use App\Exceptions\UnprocessableEntityException;
+use App\Services\Request;
 
 class Main
 {
     public function run(): void
     {
-        (new PhpInfo())->show();
+        try {
+            Request::getInstance()->process();
+        } catch (UnprocessableEntityException $e) {
+            http_response_code(422);
+            echo $e->getMessage();
+        } catch (MethodNotAllowedException $e) {
+            http_response_code(405);
+            echo $e->getMessage();
+        } catch (BadRequestException $e) {
+            http_response_code(400);
+            echo $e->getMessage();
+        } catch (\Exception $e) {
+            http_response_code(400);
+            echo $e->getMessage();
+        }
     }
 }
