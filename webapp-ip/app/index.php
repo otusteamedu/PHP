435c368ee12d3a428e3b2f5467a6d7ac6f3f3aad
Astrakhantsev Viktor <astrviktor@gmail.com>
2020-03-05 12:38:30 +0300

    fixed index.php

--- webapp-ip/app/index.php
+++ webapp-ip/app/index.php
@@ -2,34 +2,8 @@
 
     require_once 'vendor/autoload.php';
 
-    if (!isset($_POST['string']))
-    {
-        http_response_code(400);
-        echo 'Строка не получена';
-        return;
-    }
-
-    if (!isset($_SERVER['HTTP_CONTENT_LENGTH']))
-    {
-        http_response_code(400);
-        echo 'HTTP_CONTENT_LENGTH не указана';
-        return;
-    }
-
-    if (!is_numeric($_SERVER['HTTP_CONTENT_LENGTH']))
-    {
-        http_response_code(400);
-        echo 'HTTP_CONTENT_LENGTH не число';
-        return;
-    }
-
     // проверка
     $checker = new \Astrviktor\Tools\Checker\Checker();
     
-    $str = 'string=' . $_POST['string'];
-    $len = $_SERVER['HTTP_CONTENT_LENGTH'];
+    $checker->checkParenthesisPOSTandSERVER($_POST, $_SERVER);
 
-    $res = $checker->checkParenthesis($len, $str);
-
-    http_response_code($res['answer']);
-    echo $res['info'];
