a7c34d153a6ddef1bbd9c6c7affa1c581f193b84
Vlad Duplin <vlad.duplin@ya.ru>
2020-03-07 20:42:14 +0300

    HW7 В ответ по ошибке добавлено имя ответившего сервера

--- src/App.php
+++ src/App.php
@@ -3,11 +3,13 @@
 namespace Bjlag;
 
 use Nyholm\Psr7\Factory\Psr17Factory;
+use Psr\Http\Message\ResponseInterface;
 use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
 use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Exception\InvalidParameterException;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Loader\YamlFileLoader;
 use Symfony\Component\Routing\Router;
@@ -22,6 +24,9 @@ class App
 
     /** @var \Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory */
     private $psrHttpFactory;
+
+    /** @var \Psr\Http\Message\ServerRequestInterface */
+    private $request;
 
     public function __construct()
     {
@@ -39,7 +44,7 @@ class App
     public function run(): void
     {
         $symfonyRequest = Request::createFromGlobals();
-        $request = $this->psrHttpFactory->createRequest($symfonyRequest);
+        $this->request = $this->psrHttpFactory->createRequest($symfonyRequest);
 
         try {
             $parameters = $this->router->matchRequest($symfonyRequest);
@@ -48,15 +53,29 @@ class App
             $action = $parameters['_action'];
 
             /** @var \Psr\Http\Message\ResponseInterface $response */
-            $response = (new $controller())->$action($request, $parameters);
+            $response = (new $controller())->$action($this->request, $parameters);
         } catch (ResourceNotFoundException $e) {
-            $response = new \Nyholm\Psr7\Response(Response::HTTP_NOT_FOUND, [], 'Page not found');
+            $response = $this->createResponseError(Response::HTTP_NOT_FOUND, 'Page not found');
+        } catch (InvalidParameterException $e) {
+            $response = $this->createResponseError(Response::HTTP_UNPROCESSABLE_ENTITY, "Error: {$e->getMessage()}");
         } catch (\Exception $e) {
-            $response = new \Nyholm\Psr7\Response(Response::HTTP_INTERNAL_SERVER_ERROR, [], 'Server error');
+            $response = $this->createResponseError(Response::HTTP_INTERNAL_SERVER_ERROR, 'Server error');
         }
 
         (new HttpFoundationFactory())
             ->createResponse($response)
             ->send();
+    }
+
+    /**
+     * @param int $code
+     * @param string $body
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    private function createResponseError(int $code, string $body): ResponseInterface
+    {
+        return $response = (new \Bjlag\Response($code, [], $body))
+            ->withServerName($this->request)
+            ->get();
     }
 }
