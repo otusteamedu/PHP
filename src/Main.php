69cc1de32e3559ff67162aafc457e31a41ef31ad
Vlad Duplin <vlad.duplin@ya.ru>
2020-03-14 22:20:01 +0300

    HW10 Контейнер с PostgresSQL

--- src/Main.php
+++ src/Main.php
@@ -2,29 +2,10 @@
 
 namespace App;
 
-use App\Exceptions\BadRequestException;
-use App\Exceptions\MethodNotAllowedException;
-use App\Exceptions\UnprocessableEntityException;
-use App\Services\Request;
-
 class Main
 {
     public function run(): void
     {
-        try {
-            Request::getInstance()->process();
-        } catch (UnprocessableEntityException $e) {
-            http_response_code(422);
-            echo $e->getMessage();
-        } catch (MethodNotAllowedException $e) {
-            http_response_code(405);
-            echo $e->getMessage();
-        } catch (BadRequestException $e) {
-            http_response_code(400);
-            echo $e->getMessage();
-        } catch (\Exception $e) {
-            http_response_code(400);
-            echo $e->getMessage();
-        }
+        phpinfo();
     }
 }
