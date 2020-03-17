f0efd5ed2264e43f1acf8c8ceea9248bde0f3591
vlyagusha <vlyagusha@justclick.ru>
2020-03-17 10:42:44 +0300

    убрал $loader

--- public/index.php
+++ public/index.php
@@ -1,11 +1,6 @@
 <?php declare(strict_types=1);
 
-use Composer\Autoload\ClassLoader;
-
-/**
- * @var ClassLoader $loader
- */
-$loader = require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 $app = new App();
 $app->run();
