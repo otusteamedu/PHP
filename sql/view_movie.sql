cfd175323003ca35d6bcc10e7a5e17ffd425ab66
Vlad Duplin <vlad.duplin@ya.ru>
2020-03-22 19:59:41 +0300

    HW12 Вьюха для вывода заданий

--- sql/view_movie.sql
+++ sql/view_movie.sql
@@ -1,6 +1,35 @@
+---------------------
 -- View сборки служебных данных в форме (три колонки): фильм, задачи актуальные на сегодня, задачи
 -- актуальные через 20 дней.
 
+CREATE VIEW movie_tasks AS
+
+-- Задания на сегодня
+WITH tasks_today AS (
+    SELECT mav.movie_id, mav.attribute_id, ma.name
+    FROM movies_attributes_value mav
+             JOIN movies_attributes ma ON mav.attribute_id = ma.id
+    WHERE ma.is_system IS TRUE
+      AND ma.type_id = 4
+      AND date_trunc('day', mav.date_value) = date_trunc('day', now())
+),
+
+-- Задания через 20 дней
+tasks_after_20_days AS (
+    SELECT mav.movie_id, mav.attribute_id, ma.name
+    FROM movies_attributes_value mav
+             JOIN movies_attributes ma ON mav.attribute_id = ma.id
+    WHERE ma.is_system IS TRUE
+      AND ma.type_id = 4
+      AND date_trunc('day', mav.date_value) > date_trunc('day', now() + INTERVAL '20 days')
+)
+
+SELECT m.name AS movie, tt.name AS today, ta20d.name AS "after 20 days"
+FROM movies m
+    LEFT JOIN tasks_today tt ON m.id = tt.movie_id
+    LEFT JOIN tasks_after_20_days ta20d ON m.id = ta20d.movie_id;
+
+---------------------
 -- View сборки данных для маркетинга в форме (три колонки): фильм, тип атрибута, атрибут, значение
 -- (значение выводим как текст)
 
@@ -24,3 +53,4 @@ WHERE ma.is_system IS FALSE;
 -- UNDO
 
 DROP VIEW movie_attributes_view;
+DROP VIEW movie_tasks;
