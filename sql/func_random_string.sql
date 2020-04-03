401b98b07fa1c95a356ab98314536e6810260e12
Vlad Duplin <vlad.duplin@ya.ru>
2020-04-03 18:30:43 +0300

    HW13 Функция генерации случайной строки

---
+++ sql/func_random_string.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION random_string(length integer) RETURNS text AS
+$$
+DECLARE
+    chars  text[]  := '{0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}';
+    result text    := '';
+    i      integer := 0;
+BEGIN
+    IF length < 0 THEN
+        RAISE EXCEPTION 'Given length cannot be less than 0';
+    END IF;
+    FOR i IN 1..length
+        LOOP
+            result := result || chars[1 + random() * (array_length(chars, 1) - 1)];
+        END LOOP;
+    RETURN result;
+END;
+$$ LANGUAGE plpgsql;
