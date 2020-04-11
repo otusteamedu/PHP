0bb253fa80a6edb0089d99244d80bac33bcad7a2
ksenofontovs <ksenofontovsm@yandex.ru>
2020-04-11 14:05:40 +0500

    Домашнее задание 8. Исправление запроса на самый прибыльный фильм.

--- query.sql
+++ query.sql
@@ -1,5 +1,6 @@
 SELECT SUM(t.price), m.title FROM ticket t
 LEFT JOIN showtime st ON st.id = t.showtime_id
 LEFT JOIN movie m ON m.id = st.movie_id
+WHERE t.sold = true
 GROUP BY st.movie_id, m.title
 ORDER BY sum(t.price) DESC LIMIT 1;
