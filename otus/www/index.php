f0385fda97651416ab8f76e6e43cbd56a10aadc9
maxfolder <m.papka@worksolutions.ru>
2020-04-24 00:54:50 +0300

    HW1_3. Настройка memcached

--- otus/www/index.php
+++ otus/www/index.php
@@ -1,2 +1,13 @@
 <?php
-echo 'works!';
+
+require_once __DIR__. '/vendor/autoload.php';
+
+$mc = new Memcached();
+$mc->addServer("mymemcached", 11211);
+$mc->add("key1", "value1");
+$mc->add("key2", "value2");
+$mc->add("key3", "value3");
+
+echo "key1 : " . $mc->get("key1") . "\n";
+echo "key2 : " . $mc->get("key2") . "\n";
+echo "key3 : " . $mc->get("key3") . "\n";
