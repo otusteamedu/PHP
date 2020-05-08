0e1c8942f5ff7b72616a12ca906d0ee3165f2765
Denis <den002188@gmail.com>
2020-05-08 19:45:48 +0600

    fix: application validate env data

--- hw12/www/site/src/Application.php
+++ hw12/www/site/src/Application.php
@@ -13,35 +13,34 @@ use RuntimeException;
  */
 class Application {
 
+    public $envNeeds = [
+        'DB_CONNECTION',
+        'DB_HOST',
+        'DB_PORT',
+        'DB_DATABASE',
+        'DB_USERNAME',
+        'DB_PASSWORD'
+    ];
+
     public function start(): bool
     {
         (Dotenv::createImmutable(dirname(__DIR__) , '/.env'))->load();
 
-        if(!getenv('DB_CONNECTION')) {
-            throw new RuntimeException('missing from the env file "DB_CONNECTION"');
-        }
-
-        if(!getenv('DB_HOST')) {
-            throw new RuntimeException('missing from the env file "DB_HOST"');
-        }
-
-        if(!getenv('DB_PORT')) {
-            throw new RuntimeException('missing from the env file "DB_PORT"');
-        }
-
-        if(!getenv('DB_DATABASE')) {
-            throw new RuntimeException('missing from the env file "DB_DATABASE"');
-        }
-
-        if(!getenv('DB_USERNAME')) {
-            throw new RuntimeException('missing from the env file "DB_USERNAME"');
-        }
-
-        if(!getenv('DB_PASSWORD')) {
-            throw new RuntimeException('missing from the env file "DB_PASSWORD"');
-        }
+        $this->validateEnv();
 
         $router = new Route();
         $router->init();
+    }
+
+    public function validateEnv()
+    {
+        if (empty($this->envNeeds)) {
+            return;
+        }
+        foreach ($this->envNeeds as $envNeed) {
+            if (!getenv($envNeed)) {
+                throw new RuntimeException('missing from the env file "' . $envNeed . '"');
+            }
+        }
     }
 }
