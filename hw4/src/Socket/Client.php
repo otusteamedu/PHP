61848e8a7155761f6497ade5bed155333a0daa6b
deadly117 <64491750+deadly117@users.noreply.github.com>
2020-05-24 09:45:25 +0300

    client

---
+++ hw4/src/Socket/Client.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Deadly117\Socket;
+
+use Exception;
+use Socket\Raw\Factory;
+
+class Client
+{
+    private $socket;
+
+    public function __construct($endpoint)
+    {
+        if (!file_exists($endpoint)) {
+            throw new Exception('socket not found');
+        }
+
+        $factory = new Factory();
+        $this->socket = $factory->createClient("unix://{$endpoint}");
+    }
+
+    public function run()
+    {
+        $data = mt_rand();
+
+        echo 'client send: ', $data, PHP_EOL;
+        $this->socket->write($data . PHP_EOL);
+
+        while (true) {
+            $result = trim($this->socket->read(256, PHP_NORMAL_READ));
+
+            if (empty($result)) {
+                usleep(300000);
+                continue;
+            }
+
+            echo 'client recv: ', $result, PHP_EOL;
+            
+            echo 'client exiting', PHP_EOL;
+            $this->socket->write('exit' . PHP_EOL);
+            break;
+        }
+
+        $this->socket->close();
+    }
+}
