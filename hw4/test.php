6ba4b4dbedc7ed7ca9c240218ee1adfeaf9574d2
deadly117 <64491750+deadly117@users.noreply.github.com>
2020-05-24 09:45:41 +0300

    wiring

--- hw4/test.php
+++ hw4/test.php
@@ -3,5 +3,24 @@
 require 'vendor/autoload.php';
 
 $cfg = new Deadly117\Config('config.ini');
-echo $cfg->getValue('socket.file'), PHP_EOL;
-var_dump($cfg);
+$file = $cfg->getValue('socket.file');
+
+$shortopts = 'm:';
+$longopts = ['mode:'];
+$options = getopt($shortopts, $longopts);
+
+$mode = $options['m'] ?? $options['mode'];
+
+switch ($mode) {
+    case 'server':
+        $server = new Deadly117\Socket\Server($file);
+        $server->run();
+        break;
+    case 'client':
+        $client = new Deadly117\Socket\Client($file);
+        $client->run();
+        break;
+    default:
+        throw new Exception('unknown mode');
+        break;
+}
