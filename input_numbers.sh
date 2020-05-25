f535aa65a27f2c3ee4f922e3cb62ee15c47d659b
Evgenii Poperezhai <eapdob@gmail.com>
2020-05-26 02:31:56 +0300

    fix input_numbers.sh

--- input_numbers.sh
+++ input_numbers.sh
@@ -2,4 +2,18 @@
 echo Enter two numbers:;
 read num1;
 read num2;
-echo "" | awk -v num1=$num1 -v num2=$num2 '{ if (num1 != "" && num1 ~ /[+-]?([0-9]*[.])?[0-9]+/ && num2 != "" && num2 ~ /[+-]?([0-9]*[.])?[0-9]+/) { if (num1 < 0) { print num2+num1 } else if (num2 < 0) { print num1+num2 } else if (num1 > 0 || num2 > 0) { print num1+num2 } } else { print "Please try more with valida input numbers" } }';
+echo "" | awk -v num1=$num1 -v num2=$num2 '{
+  if ((num1 == "" || num1 !~ /[+-]?([0-9]*[.])?[0-9]+/) && (num2 != "" || num2 ~ /[+-]?([0-9]*[.])?[0-9]+/)) {
+   print "Please enter valid first arguments"
+  } else if ((num1 != "" || num1 ~ /[+-]?([0-9]*[.])?[0-9]+/) && (num2 == "" || num2 !~ /[+-]?([0-9]*[.])?[0-9]+/)) {
+    print "Please enter valid second arguments"
+  } else if ((num1 == "" || num1 !~ /[+-]?([0-9]*[.])?[0-9]+/) && (num2 == "" || num2 !~ /[+-]?([0-9]*[.])?[0-9]+/)) {
+    print "Please enter valid both arguments"
+  } else if (num1 < 0) {
+    print num2+num1
+  } else if (num2 < 0) {
+    print num1+num2
+  } else if (num1 > 0 || num2 > 0) {
+    print num1+num2
+  }
+}';
