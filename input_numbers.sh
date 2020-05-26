6c28ae0cdad3a14fc856cee167a94358cc708cae
Evgenii Poperezhai <eapdob@gmail.com>
2020-05-26 13:38:33 +0300

    fix input_numbers.sh

--- input_numbers.sh
+++ input_numbers.sh
@@ -3,17 +3,16 @@ echo Enter two numbers:;
 read num1;
 read num2;
 echo "" | awk -v num1=$num1 -v num2=$num2 '{
-  if ((num1 == "" || num1 !~ /[+-]?([0-9]*[.])?[0-9]+/) && (num2 != "" || num2 ~ /[+-]?([0-9]*[.])?[0-9]+/)) {
-   print "Please enter valid first arguments"
-  } else if ((num1 != "" || num1 ~ /[+-]?([0-9]*[.])?[0-9]+/) && (num2 == "" || num2 !~ /[+-]?([0-9]*[.])?[0-9]+/)) {
+  regexDigit = /[+-]?([0-9]*[.])?[0-9]+/
+  conditionFirstAgrumentValid = (num1 != "" || num1 ~ regexDigit);
+  conditionSecondAgrumentValid = (num2 != "" || num2 ~ regexDigit);
+  if (!conditionFirstAgrumentValid && conditionSecondAgrumentValid) {
+    print "Please enter valid first arguments"
+  } else if (conditionFirstAgrumentValid && !conditionSecondAgrumentValid) {
     print "Please enter valid second arguments"
-  } else if ((num1 == "" || num1 !~ /[+-]?([0-9]*[.])?[0-9]+/) && (num2 == "" || num2 !~ /[+-]?([0-9]*[.])?[0-9]+/)) {
+  } else if (!conditionFirstAgrumentValid && !conditionSecondAgrumentValid) {
     print "Please enter valid both arguments"
-  } else if (num1 < 0) {
-    print num2+num1
-  } else if (num2 < 0) {
-    print num1+num2
-  } else if (num1 > 0 || num2 > 0) {
-    print num1+num2
+  } else {
+    if (num1 < 0) { print num2+num1 } else { print num1+num2 }
   }
 }';
