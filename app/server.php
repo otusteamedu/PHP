1d22ac3c7bb750ddd39635f413d4bd60bed1635e
Alex <mar4ehk0@gmail.com>
2020-05-27 16:27:03 +0600

    added write to file for debugging

--- app/server.php
+++ app/server.php
@@ -11,7 +11,9 @@ try {
   $server = new Server($config->get("server_socket"));
   while (true) {
     $value = $server->read();
-    echo "Received: " . $value . PHP_EOL;
+    $msg = "Received: " . $value . PHP_EOL;
+    echo $msg;
+    file_put_contents('test.txt', $msg, FILE_APPEND);
     if (strtolower($value) == STOP) {
       break;
     }
