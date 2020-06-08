e2a3b624397b394df7d6615917ec9b20eda9d3a7
kibelov <kibelov@web-zaim.ru>
2020-06-08 13:09:42 +0700

    fix

--- www/app.php
+++ www/app.php
@@ -10,27 +10,9 @@ try{
     $env = Dotenv::createImmutable(dirname(__FILE__));
     $env->load();
 
-    $option = $argv[1];
+    $driver =  new Classes\Driver();
+    $driver->run($argv[1]);
 
-    switch ($option) {
-        case 'server':
-            $server = new Classes\Socket(getenv('SOCKET_ADDRESS'), getenv('SOCKET_PORT'));        
-            $server->listen();
-            
-            break;
-        
-        case 'client':
-            $client = new Classes\Client(getenv('SOCKET_ADDRESS'), getenv('SOCKET_PORT'));        
-
-            $msg = fgets(STDIN);
-
-            if (!empty($msg)) {
-                echo $client->send($msg);
-                echo $client->response() . "\n";
-            } 
-
-            break;
-    }
 } catch (Exception $ex) {
     echo $ex->getMessage();
 }
