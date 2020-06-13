fc6ca19230f5aa90b8704538983fca1db1ff7004
prinzplanloser0514 <prinzplanloser0514@gmail.com>
2020-06-13 17:38:48 +0300

    Домашнее задание №6

---
+++ hw6/task1/app/validation.php
@@ -0,0 +1,22 @@
+<?php
+
+$value = $_POST['email'];
+$validator = new \App\Services\Validator($value);
+
+$validator->notEmpty();
+$validator->addRegexp("<\S{1,}@\S{2,}\.\S{2,}>");
+$validator->validateMx();
+$validator->validateEmail();
+
+$validationErrors = $validator->getErrors();
+
+if ($validationErrors) {
+    http_response_code(400);
+    echo 'Ошибка - ваша почта не прошла валидацию' . PHP_EOL . PHP_EOL;
+    foreach ($validationErrors as $error) {
+        echo $error . PHP_EOL;
+    }
+} else {
+    http_response_code(200);
+    echo 'Валидация прошла успешно.';
+}
