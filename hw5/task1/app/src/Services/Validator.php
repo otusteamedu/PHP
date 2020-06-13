3cecbc2b2d92bda1f9c9fcdd6688fed0b9f64d9b
prinzplanloser0514 <prinzplanloser0514@gmail.com>
2020-06-13 11:53:22 +0300

    Домашнее задание № 5.

---
+++ hw5/task1/app/src/Services/Validator.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Services;
+
+class Validator {
+
+    private $value;
+    private $errors;
+
+    public function __construct($value = null) {
+        $this->value = $value;
+    }
+
+    public function notEmpty(): void {
+        if (!$this->value) {
+            $this->errors['empty'] = 'Не передано значение';
+        }
+    }
+
+    public function addRegexp(string $regexp): void {
+        $pregResult = preg_match($regexp, $this->value);
+        if ($pregResult === 0) {
+            $this->errors[] = 'Значение не соответствует регулярному выражению';
+        }
+    }
+
+    public function length(int $length): void {
+        if (strlen($this->value) !== $length) {
+            $this->errors[] = sprintf('Длинна должна быть равна %s байт', $length);
+        }
+    }
+
+    public function countingSymbols(string $symbol, int $count): void {
+        $repetition = substr_count($this->value, $symbol);
+        if ($repetition !== $count) {
+            $this->errors[] = sprintf('Символ "%s" встречается %s раз,а должен %s', $symbol, $repetition, $count);
+        }
+    }
+
+    public function getErrors(): array {
+        if (isset($this->errors['empty'])) {
+            return [$this->errors['empty']];
+        }
+        return $this->errors;
+    }
+}
