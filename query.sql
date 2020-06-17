d54d9449728e27238399c73c70c877f968c793dd
Alexey Korchagin <akorchaginn@gmail.com>
2020-06-18 02:45:18 +0700

    Кинотеатр

--- query.sql
+++ query.sql
@@ -1,11 +1,10 @@
 /**
  * Самый собираемый фильм в данной сети кинотеатров
  */
-select m2.id, m2."name", sum(ss.rate) as box_office, sum((1 - cast (oi.discount as float) / 100) * ss.rate) as box_office_with_discount
+select m2.id, m2."name", sum(o.total) as total
 from movie m2
-inner join "session" s on s.movie_id = m2.id
-inner join session_seat ss on ss.session_id = s.id
-inner join order_item oi on oi.session_seat_id = ss.id
+     inner join "session" s on s.movie_id = m2.id
+     inner join "order" o on o.session_id = s.id
 group by m2.id
-order by box_office_with_discount desc
+order by total desc
 limit 1
