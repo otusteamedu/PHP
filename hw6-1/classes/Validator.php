4978ec15ca48b15bd82f3fe43ba1f86bf42ab9f6
kibelov <kibelov@web-zaim.ru>
2020-06-29 02:22:21 +0700

    fix

--- hw6-1/classes/Validator.php
+++ hw6-1/classes/Validator.php
@@ -3,22 +3,13 @@
 namespace Classes;
 
 class Validator {
-    private $brackets = [
-        ['{', '}'],
-        ['(', ')'],
-        ['[', ']']
-    ];
+    private $reg = "#^[^()\n]*+(\((?>[^()\n]|(?1))*+\)[^()\n]*+)++$#";
 
     public function __construct() {}
 
     public function isCorrect (String $string) : Bool {
-        $string = trim($string);
-
-        foreach($this->brackets AS $bracket){
-            if( substr_count($string, $bracket[0]) != substr_count($string, $bracket[1]) )
-                return false;
-        }
-        
-        return true;
+        return (preg_match($this->reg, $string)) 
+            ? true
+            : false;
     }
 }
--- hw6-1/index.php
+++ hw6-1/index.php
@@ -1,5 +1,10 @@
 <?php
 
+include_once('vendor/autoload.php');
+
 $v = new \Classes\Validator();
-var_dump($v->isCorrect('({})'));
+
+echo ($v->isCorrect('(()())'))
+    ? 'correctly'
+    : 'incorrectly';
 
