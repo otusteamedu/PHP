e5885d589d8973a22d34db8413311118ef8b64d8
prinzplanloser0514 <prinzplanloser0514@gmail.com>
2020-06-28 16:43:42 +0300

    Домашнее задание №10

--- ddl.sql
+++ ddl.sql
@@ -173,6 +173,23 @@ FROM films
          WHERE av.date::date = current_date + interval '20 day'
      ) t2 ON films.id = t2.film_id;
 
+Create or replace function "random_string"(length integer) returns text as
+$$
+declare
+    chars text[] := '{0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}';
+    result text := '';
+    i integer := 0;
+begin
+    if length < 0 then
+        raise exception 'Неверная длинна';
+    end if;
+    for i in 1..length loop
+            result := result || chars[1+random()*(array_length(chars, 1)-1)];
+        end loop;
+    return result;
+end;
+$$ language plpgsql;
+
 ALTER TABLE "session"
     ADD CONSTRAINT "lnk_film_session" FOREIGN KEY ("film_id")
         REFERENCES "films" ("id") MATCH FULL
