66b2a2216b4f6022440fc6ea8c55d47b3dfe679e
maxfolder <m.papka@worksolutions.ru>
2020-07-23 18:01:18 +0300

    hw_2_5.activeRecord. Реализация

---
+++ otus/www/classes/Database/DbException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Classes\Database;
+
+class DbException extends \Exception
+{
+
+}
--- otus/www/index.php
+++ otus/www/index.php
@@ -1,5 +1,22 @@
 <?php
 
-//require_once 'vendor/autoload.php';
+require_once 'vendor/autoload.php';
 
-echo 'db_start';
+$filmId = 1;
+
+//////Обновление записи в таблице
+$newFilm = \Classes\Models\Film::find($filmId);
+$newFilm->setName('test 1');
+$newFilm->save();
+
+//Создание записи в таблице
+$newRow = new \Classes\Models\Film();
+$newRow->setName("Max Folder");
+$newRow->save();
+
+////
+////Удаление записи в таблице
+/** @var \Classes\Models\Film $rowObj */
+$rowObj = \Classes\Models\Film::find($filmId);
+$rowId = $rowObj->getId();
+\Classes\Models\Film::delete($rowId);
