54853251e1a3d4c8c155cbc4a03892c4431fa00f
Azat Nizameev <azat.nizam@gmail.com>
2020-07-26 00:58:43 +0300

    1) run db migrator, 2) description

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,14 +1,3 @@
-# server-based syntax
-# ======================
-# Defines a single server with a list of roles and multiple properties.
-# You can define all roles on a single server, or split them:
-
-# server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
-# server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
-# server "db.example.com", user: "deploy", roles: %w{db}
-
-
-
 # role-based syntax
 # ==================
 
@@ -18,19 +7,6 @@
 # Don't use `:all`, it's a meta role.
 
 role :app, %w{otus@otus.dev-code.ru}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
-
-
-
-# Configuration
-# =============
-# You can set any configuration variable like in config/deploy.rb
-# These variables are then only loaded and set in this stage.
-# For available Capistrano configuration variables see the documentation page.
-# http://capistranorb.com/documentation/getting-started/configuration/
-# Feel free to add new variables to customise your setup.
-
 
 
 # Custom SSH Options
@@ -45,19 +21,6 @@ set :ssh_options, {
     forward_agent: false,
     auth_methods: %w(password)
 }
-#
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server "otus.dev-code.ru",
-#   user: "otus",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "otus", # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
 
 namespace :deploy do
     desc "Check that we can access everything"
@@ -90,6 +53,15 @@ namespace :deploy do
             within release_path do
                 info "...Run Composer..."
                 execute :composer, "install --no-dev"
+            end
+        end
+    end
+
+    after :updated, :migrate do
+        on roles(:all) do
+            within release_path do
+                info "...Migrations..."
+                execute :php, "console/migrator.php"
             end
         end
     end
