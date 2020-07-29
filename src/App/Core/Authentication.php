042b37a2a7e9733b091d9a85bd48bed55fe665a1
Ozy <Ozy@nxt.ru>
2020-07-29 13:47:46 +0300

    Имя ключа для аутентификации перенесено в настройки

--- src/App/Core/Authentication.php
+++ src/App/Core/Authentication.php
@@ -6,16 +6,16 @@ use Ozycast\App\Models\Client;
 
 class Authentication
 {
-    const API_KEY = "Api-Key";
-
     /**
      * Вернет найденного пользователя
      * @return DTO|null
      */
     public static function check()
     {
+        $api_key_name = $_ENV["AUTHENTICATION_KEY_NAME"];
+
         $header = getallheaders();
-        $api_key = $header[self::API_KEY];
+        $api_key = $header[$api_key_name];
 
         $user = Client::findByKey($api_key);
         return $user;
