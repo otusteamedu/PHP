d00660f2b925e12dc379ca0177f4349bc66d54e7
kibelov <kibelov@web-zaim.ru>
2020-08-16 16:09:50 +0700

    done

--- public/index.php
+++ public/index.php
@@ -7,9 +7,13 @@ require_once 'vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-(new Spider(new \Sources\Animevost(), new \DB\Mongodb()))
-    ->getTop()
-    ->save();
+try {
+    (new Spider(new \Sources\Animevost(), new \DB\Mongodb()))
+        ->getTop()
+        ->save();
+} catch (Exception $e) {
+    echo 'Error: ',  $e->getMessage(), "\n";
+}
 
 
 
