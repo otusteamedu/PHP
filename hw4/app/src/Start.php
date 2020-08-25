3be3909d6a4021cecf2a9fe55d9ee202ee6fffb8
Andrey_Pirozhkov_196 <anderpirozhkov@yandex.ru>
2020-08-25 14:28:55 +0500

    Change index file

---
+++ hw4/app/src/Start.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace App;
+
+
+use Exception;
+
+class Start
+{
+    const TYPE_SERVER = "server";
+    const TYPE_CLIENT = "client";
+
+    private $shortOpts = "t:m:";
+    private $longOpts = ["type:", "message:"];
+    private $options;
+    private $type;
+    private $message;
+
+    public function __construct($iniPath)
+    {
+        $this->options = getopt($this->shortOpts, $this->longOpts);
+
+        $this->type = $this->options['t'] ?? $this->options['type'];
+        $this->message = $this->options['m'] ?? $this->options['message'];
+
+        try{
+            $this->validateOptions();
+        } catch (Exception $exception) {
+            echo $exception->getMessage();
+            exit();
+        }
+
+        $this->startSession($iniPath);
+    }
+    private function validateOptions()
+    {
+        if (empty($this->type)) {
+            throw new Exception(
+                'You must specify the type via the t parameter or the type. Valid values are client or server'.PHP_EOL
+            );
+        }
+
+        if ($this->type !== self::TYPE_SERVER && $this->type !== self::TYPE_CLIENT) {
+            throw new Exception('Valid values for client or server type'.PHP_EOL);
+        }
+
+        if ($this->type === self::TYPE_CLIENT && empty($this->message)) {
+            throw new Exception('You forgot to enter a message to the server'.PHP_EOL);
+        }
+    }
+
+    private function startSession($iniPath)
+    {
+        $class = "App\\" . ucfirst($this->type);
+        (new $class($iniPath))->start($this->type !== 'client' ?: $this->message);
+    }
+}
