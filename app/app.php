8f3f4ced249c9c2995b8f7d96523e5be3fe03c96
Evgenii Poperezhai <eapdob@gmail.com>
2020-08-28 11:26:00 +0300

    removed .idea folders bootstrap autoload fix app fix

--- app/app.php
+++ app/app.php
@@ -2,10 +2,12 @@
 
 define('ROOT', dirname(__DIR__));
 
-require ROOT . '/vendor/autoload.php';
-require ROOT . '/bootstrap/app.php';
+require __DIR__ . '/../bootstrap/app.php';
 
 use Otus\Http\App;
+use Otus\Sockets\Exceptions\SocketException;
+use Otus\Sockets\Server;
+use Otus\Sockets\Client;
 
 // 4.1
 if ($argv[1] === '') {
@@ -18,9 +20,20 @@ if ($argv[1] === '') {
 }
 
 // 4.2
-if ($argv[1] !== '') {
-    $file = ROOT . '/sockets/' . $argv[1] . '.php';
-    if (file_exists($file)) {
-        require $file;
+if ($argv[1] === 'server') {
+    try {
+        $server = new Server($_ENV['SOCKET_PATH'], $_ENV['SOCKET_PORT']);
+        $server->listen();
+    } catch (SocketException $e) {
+        echo 'SocketException ' . PHP_EOL;
+    }
+}
+
+if ($argv[1] === 'client') {
+    try {
+        $client = new Client($_ENV['SOCKET_PATH'], $_ENV['SOCKET_PORT']);
+        $client->connect();
+    } catch (SocketException $e) {
+        echo 'SocketException ' . PHP_EOL;
     }
 }
