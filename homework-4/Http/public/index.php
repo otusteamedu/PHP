eb885df74419a409a3b5898a4b4afe352bae3619
Viacheslav Ostrovskiy <chelout@gmail.com>
2020-09-07 11:22:43 +0300

    Вынес объявление роутов в файл настроек

--- homework-4/Http/public/index.php
+++ homework-4/Http/public/index.php
@@ -7,9 +7,4 @@ use Otus\Http\Request;
 use Otus\Http\Router;
 
 $router = new Router();
-$router->addRoute('POST', '/', [Controller::class, 'index']);
-
-$request  = Request::capture();
-$response = $router->handle($request);
-
-$response->send();
+$router->handle()->send();
