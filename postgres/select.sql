e2125ef950e2953ed40d4acfc46860f00f2bd53a
Vadim Kasatkin <penguin2804@gmail.com>
2020-09-08 00:35:56 +0400

    change select

--- postgres/select.sql
+++ postgres/select.sql
@@ -1,1 +1,1 @@
-SELECT f.id, f.name, SUM(s.price) as total FROM tickets as t JOIN sessions as s ON t.session_id = s.id JOIN films as f ON s.film_id = f.id GROUP BY f.id ORDER BY total DESC LIMIT 1;
+SELECT f.id, f.name, SUM(t.cost) as total FROM tickets as t JOIN sessions as s ON t.session_id = s.id JOIN films as f ON s.film_id = f.id GROUP BY f.id ORDER BY total DESC LIMIT 1;
