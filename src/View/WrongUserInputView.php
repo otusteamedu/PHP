5f289224ecd7f79cce17e92d9be45c3becfbf428
IS Glushkov <53116914+GlushkovIS@users.noreply.github.com>
2020-09-20 23:52:24 +0300

    fix

--- src/View/WrongUserInputView.php
+++ src/View/WrongUserInputView.php
@@ -6,7 +6,6 @@ class WrongUserInputView extends View
 {
     public function output()
     {
-        parent::output();
         echo '<h5>Имя канала (пользователя) не может быть более 150 символов</h5>';
     }
 }
