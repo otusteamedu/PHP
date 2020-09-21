a8cacc109cca33edb37f52748e1e22ad297d3694
Petr Ivanov <petr.yrs@gmail.com>
2020-09-21 23:13:42 +0300

    populate.sh - сортирвка по убыванию sum.sh - запрашиваем корректные данные

--- hw1/Scripts/populate.sh
+++ hw1/Scripts/populate.sh
@@ -2,4 +2,4 @@
 
 fileName=$1
 
-cat $fileName | tail -n +2 | awk -F " " '{print $3}' | sort | uniq -c | head -n 3 | sort
+cat $fileName | tail -n +2 | awk -F " " '{print $3}' | sort | uniq -c | head -n 3 | sort -r
