2f0d2d5e25814ea6be92fffda1706d1ff7739a09
alex-antonov <antonovsasha22@mail.ru>
2020-10-01 23:41:21 +0700

    ДЗ №14. SQL скрипты с инициализацией базы перенесены в скрипты инициализации контейнера.

--- hw-14/SQL/DDL/01_create_films_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE films
-(
-    id          serial      not null
-        constraint films_pkey primary key,
-    name        varchar(60) not null,
-    description text        not null,
-    duration    integer     not null,
-    age_limit   smallint
-);
--- hw-14/SQL/DDL/02_create_hall_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE halls
-(
-    id           serial not null
-        constraint halls_pkey primary key,
-    name         varchar(40),
-    max_visitors integer
-);
