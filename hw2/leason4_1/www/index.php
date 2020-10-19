e7af2625c3218ec7747bea1b3f70b9dee6892946
Petr Ivanov <petr.yrs@gmail.com>
2020-10-19 17:39:28 +0300

    4.1 используем загрузчик из Composer

--- hw2/leason4_1/www/index.php
+++ hw2/leason4_1/www/index.php
@@ -1,23 +1,9 @@
 <?php
-include 'src/autoload.php';
+include 'vendor/autoload.php';
 
-const minLength = 20;
-
-$request  = new HttpRequest();
-$response = new HttpResponse();
-
-if ($request->isPost()) {
-    $data = $request->getParam('string');
-    if ( ! empty($data) && strlen($data) > minLength) {
-        $checker = new Checker();
-        if ($checker->check($data)) {
-            $response->send(200, 'Последовательность верная');
-        } else {
-            $response->send(500, 'Последовательность не верная');
-        }
-    } else {
-        $response->send(500, 'Не корректная длина строки');
-    }
-} else {
-    $response->send(500, 'Необходимо отправить POST-запрос');
+try {
+    $app = new App();
+    $app->run();
+} catch (Exception $e) {
+    echo $e->getMessage();
 }
--- hw2/leason4_1/www/src/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-function autoloader($class){
-    $fullFileName = __DIR__.'/'.$class.'.php';
-    if (file_exists($fullFileName)) {
-        include $fullFileName;
-    }
-}
-
-spl_autoload_register('autoloader');
