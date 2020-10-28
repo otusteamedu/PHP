6c7e756ca891276cb49c16beaeecd0fb7426b647
nemroq <nemywolf@gmail.com>
2020-10-28 09:03:20 +0300

    исправил запрос на генерацию базы данных

--- test_data_generation.sql
+++ test_data_generation.sql
@@ -2,13 +2,13 @@
 insert into halls(name, site_count)
 select array_to_string(
                array(select chr((ascii('A') + round(random() * 25))::integer)
-                     from generate_series(1, 10)), ''),
+                     from generate_series(1, 10 + 0 * x)), ''),
        int2((50 + random() * 100)::integer)
-from generate_series(1, 10);
+from generate_series(1, 100000) as x;
 
 /*тестовые вызовы для проверки данных*/
-select *
-from halls;
+select name
+from halls group by name;
 
 delete
 from halls;
