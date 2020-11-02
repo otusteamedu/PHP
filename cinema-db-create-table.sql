71beba02f54719b9d3cf10a021ffe54dbf8dc2b2
nemroq <nemywolf@gmail.com>
2020-11-02 16:43:07 +0300

    Немного исправил структуру генерации базы, добавил запрос на генерацию тестовых данных, добавил планы работы БД при выполнении выборок по тестовым данным.

--- cinema-db-create-table.sql
+++ cinema-db-create-table.sql
@@ -1,7 +1,7 @@
 /* Таблица залов */
 CREATE TABLE halls
 (
-    id         integer PRIMARY KEY,
+    id         serial PRIMARY KEY,
     name       text,
     site_count integer
 );
@@ -9,24 +9,25 @@ CREATE TABLE halls
 /* Таблица фильмов */
 CREATE TABLE films
 (
-    id   integer PRIMARY KEY,
+    id   serial PRIMARY KEY,
     name text
 );
 
 /* Таблица сеансов, на которых показывают фильмы */
 CREATE TABLE sessions
 (
-    id            integer PRIMARY KEY,
+    id            serial PRIMARY KEY,
     halls_id      integer REFERENCES halls (id),
     film_id       integer REFERENCES films (id),
-    sessions_time date,
+    sessions_date date,
+    sessions_time time,
     price         money
 );
 
 /* Билеты на сеансы */
 CREATE TABLE tickets
 (
-    id          integer PRIMARY KEY,
+    id          serial PRIMARY KEY,
     sessions_id integer REFERENCES sessions (id),
     sold        boolean,
     site_number integer
@@ -34,44 +35,42 @@ CREATE TABLE tickets
 
 create table attributes
 (
-    id       integer PRIMARY KEY,
+    id       serial PRIMARY KEY,
     films_id integer REFERENCES films (id),
     name     text
 );
 
 create table attributes_type
 (
-    id              integer PRIMARY KEY,
+    id              serial PRIMARY KEY,
     attributes_id   integer REFERENCES attributes (id),
     attributes_type text
 );
 
 create table attributes_value
 (
-    id                 integer PRIMARY KEY,
+    id                 serial PRIMARY KEY,
     attributes_type_id integer REFERENCES attributes_type (id),
     film_id            integer REFERENCES films (id),
     attributes_text    text,
     attributes_integer integer,
-    attributes_file    bytea,
     attributes_float   float,
-    attributes_data    date,
-    attributes_money   money
+    attributes_data    date
 );
 
-create view(system_data) as
+create view system_data as
 select f.name, a.name, av.attributes_text
 from films as f
-         inner join attributes a on films.id = a.films_id
+         inner join attributes a on f.id = a.films_id
          inner join attributes_value av on f.id = av.film_id
 where a.name = 'current_task'
-   OR (a.name = 'future_task' AND av.attributes_data = now() + integer '20')
+   OR (a.name = 'future_task' AND av.attributes_data = (current_date + integer '20'))
 group by f.name;
 
-create view(market_data) as
+create view market_data as
 select f.name, a.name, av.attributes_text, av.attributes_data
 from films as f
-         inner join attributes a on films.id = a.films_id
+         inner join attributes a on f.id = a.films_id
          inner join attributes_value av on f.id = av.film_id
 where a.name LIKE ['жанр', 'дата премьеры', 'рейтинг']
-group by f.name
+group by f.name;
--- test_data_generation.sql
+++ test_data_generation.sql
@@ -4,11 +4,4 @@ select array_to_string(
                array(select chr((ascii('A') + round(random() * 25))::integer)
                      from generate_series(1, 10 + 0 * x)), ''),
        int2((50 + random() * 100)::integer)
-from generate_series(1, 100000) as x;
-
-/*тестовые вызовы для проверки данных*/
-select name
-from halls group by name;
-
-delete
-from halls;
+from generate_series(1, 10000000) as x;
