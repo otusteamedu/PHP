39126f74a5127fbf54781529a82081db9cb14746
nemroq <nemywolf@gmail.com>
2020-11-14 11:34:56 +0300

    Добавил работу с удалением и обновлением данных в индексе, добавил вызовы всех функций для работы извне.

--- src/App.php
+++ src/App.php
@@ -2,9 +2,17 @@
 
 
 use Elasticsearch\ClientBuilder;
+use src\ElasticIndex;
 
 class App
 {
+    public $query;
+
+    public function __construct()
+    {
+        $query = $this->query;
+    }
+
     public function run()
     {
         $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
@@ -12,8 +20,33 @@ class App
         $host = $_ENV['elastic_host'];
     }
 
-    public function createClient()
+    public function add($query)
     {
-        return ClientBuilder::create()->setHosts($host)->build();
+        $index = new ElasticIndex();
+        $index->addToIndex($query);
+    }
+
+    public function get($query)
+    {
+        $index = new ElasticIndex();
+        $index->getFromIndex($query);
+    }
+
+    public function update($query)
+    {
+        $index = new ElasticIndex();
+        $index->updateInIndex($query);
+    }
+
+    public function delete($query)
+    {
+        $index = new ElasticIndex();
+        $index->deleteFromIndex($query);
+    }
+
+    public function search($query)
+    {
+        $index = new ElasticIndex();
+        $index->search($query);
     }
 }
--- src/ElasticIndex.php
+++ src/ElasticIndex.php
@@ -6,23 +6,88 @@ namespace src;
 
 class ElasticIndex extends ElasticClient
 {
-    public function addToIndex(array $query)
+    public function addToIndex(array $query): void
     {
         $client = $this->createClient();
-        return $response = $client->index($query);
+        $params = [
+            'index' => 'youtube',
+            'id' => 1,
+            'body' => [
+                'channel_name' => $query['channel_name'],
+                'channel_subscribe' => $query['channel_subscribe'],
+                'video_name' => $query['video_name'],
+                'video_data' => $query['video_data'],
+                'video_description' => $query['video_description'],
+                'video_view' => $query['video_view'],
+                'video_like' => $query['video_like'],
+                'video_dislike' => $query['video_dislike']
+            ]
+        ];
+        $client->index($params);
     }
 
-    public function getForIndex(array $query)
+    public function getFromIndex(int $query): array
     {
         $client = $this->createClient();
-        return $response = $client->get($query);
+        $params = [
+            'index' => 'youtube',
+            'type' => 'youtube',
+            'id' => $query,
+        ];
+        return $client->get($params);
 
     }
 
-    public function search(array $query)
+    public function updateInIndex(array $query): void
     {
         $client = $this->createClient();
-        return $response = $client->search($query);
+        $param = [
+            'index' => 'youtube',
+            'id' => $query['id'],
+            'body' => [
+                'channel_name' => $query['channel_name'],
+                'channel_subscribe' => $query['channel_subscribe'],
+                'video_name' => $query['video_name'],
+                'video_data' => $query['video_data'],
+                'video_description' => $query['video_description'],
+                'video_view' => $query['video_view'],
+                'video_like' => $query['video_like'],
+                'video_dislike' => $query['video_dislike']
+            ]
+        ];
+        $client->updateByQuery($param);
+    }
+
+    public function deleteFromIndex(int $query): void
+    {
+        $client = $this->createClient();
+        $param = [
+            'index' => 'youtube',
+            'id' => $query
+        ];
+        $client->deleteByQuery($param);
+    }
+
+    public function search(string $query): array
+    {
+        $client = $this->createClient();
+        $params = [
+            'index' => 'youtube',
+            'body' => [
+                'query' => [
+                    'multi_match' => [
+                        'query' => $query,
+                        'fields' => [
+                            'channel_name^5',
+                            'video_name^4',
+                            'video_data',
+                            'video_description^2'
+                        ]
+                    ]
+                ]
+            ]
+        ];
+        return $response = $client->search($params);
     }
 
 }
