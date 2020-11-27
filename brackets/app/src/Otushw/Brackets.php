ab8cedb376f416ed03663d50c6dfd151c14b01c5
Alex <mar4ehk0@gmail.com>
2020-11-27 11:48:06 +0300

    hw2-1, fixed by code review

--- brackets/app/src/Otushw/Brackets.php
+++ brackets/app/src/Otushw/Brackets.php
@@ -6,36 +6,55 @@ use Exception;
 
 class Brackets
 {
-    const LENGTH_PAIR_BRACKETS = 2;
-    const CORRECT_BRACKETS = '()';
-
     protected $parameter;
 
-    public function validation()
+    const LENGTH_PAIR_BRACKETS = 2;
+    const CORRECT_BRACKETS = '()';
+    const PARAMETER_NAME = 'string';
+    const HTTP_200 = 200;
+    const HTTP_404 = 404;
+
+    public function __construct()
+    {
+        try {
+            if ($this->validationParameter()) {
+                $this->parameter = $_POST[self::PARAMETER_NAME];
+
+                $this->checkBrackets();
+                $code = self::HTTP_200;
+                $msgError = '';
+            }
+        } catch (Exception $e) {
+            $code = self::HTTP_404;
+            $msgError = $e->getMessage();
+        }
+
+        $this->response($code, $msgError);
+    }
+
+    protected function response($code, $msgError)
     {
         $msg = 'Validation';
-        try {
-            if (!isset($_POST['string'])) {
-                throw new Exception('Parameter "string" is missing');
-            }
-            if (empty($_POST['string'])) {
-                throw new Exception('Parameter "string" is empty');
-            }
-            if (!is_string($_POST['string'])) {
-                throw new Exception('Parameter is not string');
-            }
-
-            $this->parameter = $_POST['string'];
-
-            $this->checkBrackets();
-            $code = 200;
-            $msg .= ' passed';
-        } catch (Exception $e) {
-            $code = 404;
-            $msg .= ' failed:' . $e->getMessage();
+        $msg_suffix = ' passed';
+        if ($code == self::HTTP_404) {
+            $msg_suffix = ' failed:' . $msgError;
         }
         http_response_code($code);
-        echo $msg;
+        echo $msg . $msg_suffix;
+    }
+
+    protected function validationParameter()
+    {
+        if (!isset($_POST[self::PARAMETER_NAME])) {
+            throw new Exception('Parameter "string" is missing');
+        }
+        if (empty($_POST[self::PARAMETER_NAME])) {
+            throw new Exception('Parameter "string" is empty');
+        }
+        if (!is_string($_POST[self::PARAMETER_NAME])) {
+            throw new Exception('Parameter is not string');
+        }
+        return true;
     }
 
     protected function checkBrackets()
