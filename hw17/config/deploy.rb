2670c206ba0074a112f2a0e4e85f4c6f9cf03a0a
Alexandr Yakovlev <leffalt@gmail.com>
2020-12-02 20:16:00 +0700

    modify deploy.rb

--- hw17/config/deploy.rb
+++ hw17/config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
-set :application, "my_app_name"
+set :application, "otus_app_deployscript"
 set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default branch is :master
