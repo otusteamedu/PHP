31e1e22fa2c6ec1121bd16cab76c3e980cd040d4
yessaliyev <40271311+yessaliyev@users.noreply.github.com>
2020-12-03 16:34:02 +0600

    done

---
+++ src/class/Bracket.php
@@ -0,0 +1,26 @@
+<?php 
+
+class Bracket {
+
+    public function check($request) 
+    {
+        if ($this->validate($request)) {
+            header('HTTP/1.1 200 OK');
+            return;
+        }
+
+        header('HTTP/1.1 400 Bad request');
+    }
+
+    private function validate($brackets) : bool
+    {
+        if (!empty(trim($brackets['string']))) {
+            $result = array_count_values(str_split($brackets['string']));
+            if ( isset($result['(']) and isset($result[')']) and $result['('] === $result[')'] ) {
+                return true;
+            } 
+        }
+        
+        return false;
+    }
+}
--- src/index.php
+++ src/index.php
@@ -1,2 +1,6 @@
 <?php
 
+require_once 'class/Bracket.php';
+
+$bracket = new Bracket();
+$bracket->check($_POST);
