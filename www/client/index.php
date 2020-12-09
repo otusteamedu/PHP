1cce20fb0d114aa4279da217d066a9b79deb6612
kibelov <kibelov@web-zaim.ru>
2020-12-09 20:21:08 +0700

    fix

--- www/client/index.php
+++ www/client/index.php
@@ -1,26 +1,15 @@
 <?php
 
 use Symfony\Component\Dotenv\Dotenv;
-use Bramus\Router\Router;
-use Controllers\ApiController;
+use Controllers\RouterController;
 
 include_once(__DIR__ . '/vendor/autoload.php');
 
 (new Dotenv())->load(__DIR__ . '/.env');
 
 try {
-    $route = new Router();
-    $api = new ApiController();
-
-    $route->get('/insert', function() use ($api){
-        echo json_encode($api->insert());
-    });
-
-    $route->get('/get/{id}', function(string $id) use ($api){
-        echo json_encode($api->get($id)); 
-    });
-
-    $route->run();
+    (new RouterController())
+        ->run();
 } catch (Exception $e) {
    echo json_encode(['error' => 'Выброшено исключение: ' . $e->getMessage() ]);
 }
