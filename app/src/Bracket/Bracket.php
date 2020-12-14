1bd181d255bcfc90c9ab3544bd830f5a0fd58f51
yessaliyev <40271311+yessaliyev@users.noreply.github.com>
2020-12-14 13:49:43 +0600

    refactor

--- app/src/Bracket/Bracket.php
+++ app/src/Bracket/Bracket.php
@@ -15,15 +15,29 @@ class Bracket {
     }
 
     //вернет тру если примет ))((
-    private function validate($brackets) : bool
+    private function validate($brakets) : bool
     {
-        if (!empty(trim($brackets['string']))) {
-            $result = array_count_values(str_split($brackets['string']));
-            if ( isset($result['(']) and isset($result[')']) and $result['('] === $result[')'] ) {
-                return true;
-            } 
+    
+        if (empty(trim($brakets['string']))) {
+            return false;
+        }
+
+        if ( (substr($brakets['string'], 0, 1) == ')') || (substr($brakets['string'], -1, 1) == '(') ) {
+            return false;
+        }
+
+        foreach (str_split($brakets['string']) as $value) {
+
+            if ($value == '(') {
+                $arr[] = $value;
+                continue;
+            }
+
+            if ($value == ')' and (empty($arr) or array_pop($arr) != '(')) {
+                return false;
+            }
         }
         
-        return false;
+        return empty($arr);
     }
 }
