bafdc030f24b9a1e7eed4b724d8e013078eb0e86
Sergey Zelenskiy <zelenskiy@ffin.kz>
2020-12-17 13:29:22 +0600

    validation

--- hw-4-1/app/src/Validators/BracketsValidator.php
+++ hw-4-1/app/src/Validators/BracketsValidator.php
@@ -2,17 +2,80 @@
 
 namespace Validators;
 
-class BracketsValidator {
-
+class BracketsValidator
+{
+    /**
+     * @var string
+     */
     private string $_string;
 
+    /**
+     * BracketsValidator constructor.
+     *
+     * @param string $string
+     */
     public function __construct (string $string)
     {
-        $this->_string = $string;
+        $this->_string = trim($string);
     }
 
-    public function validate(): bool
+    /**
+     * Валидация и отдача header
+     */
+    public function validate (): void
     {
-        return false;
+        if ($this->_checkLength() === false || $this->_checkBrackets() === false) {
+            header('HTTP/1.1 400 Bad request');
+        }
+        else {
+            header('HTTP/1.1 200 OK');
+        }
+    }
+
+    /**
+     * Валидация на длину
+     *
+     * @return bool
+     */
+    private function _checkLength (): bool
+    {
+        if (strlen($this->_string) < 2) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Валидация скобок
+     *
+     * @return bool
+     */
+    private function _checkBrackets (): bool
+    {
+        if (strlen(str_replace(['(', ')'], '', $this->_string)) > 0) {
+            return false;
+        }
+
+        if ((substr($this->_string, 0, 1) === ')') || (substr($this->_string, -1, 1) === '(')) {
+            return false;
+        }
+
+        $bracketsContainer = [];
+
+        foreach (str_split($this->_string) as $bracket) {
+            if ($bracket === '(') {
+                $bracketsContainer[] = $bracket;
+                continue;
+            }
+
+            if (count($bracketsContainer) === 0) {
+                return false;
+            }
+
+            array_pop($bracketsContainer);
+        }
+
+        return true;
     }
 }
