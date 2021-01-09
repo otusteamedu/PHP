b746e386589f7aaa5308c3a8689a130aa886607e
Alex <mar4ehk0@gmail.com>
2021-01-09 18:40:09 +0300

    hw4-1 added logs

--- app/public/index.php
+++ app/public/index.php
@@ -3,11 +3,17 @@
 require __DIR__ .  '/../bootstrap/bootstrap.php';
 
 use Otushw\App;
+use Otushw\View;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 
 try {
     $app = new App();
     $app->run();
 }
 catch (Exception $e) {
-    var_dump($e->getMessage());
+    View::showClient();
+    $log = new Logger('app');
+    $log->pushHandler(new StreamHandler('app.log'));
+    $log->error($e->getMessage());
 }
