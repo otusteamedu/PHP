54651e13d80a5c277b58d8d7856fd74f4ca70ad1
genamalugin <genamalugin@gmail.com>
2021-01-14 09:18:18 +0300

    hw 4.2 socket

---
+++ hw-4/hw-4-2/vendor/composer/installed.php
@@ -0,0 +1,81 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'cdfc99e7bf8d4c9676aa3aa3b42509938297ac3d',
+    'name' => 'gena/web_mob_tuts',
+  ),
+  'versions' => 
+  array (
+    'gena/web_mob_tuts' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cdfc99e7bf8d4c9676aa3aa3b42509938297ac3d',
+    ),
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '2.x-dev',
+      ),
+      'reference' => '78bd7bd33313c3a7ad1f2b0fc0c11a203d4e3826',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '1.1.x-dev',
+      ),
+      'reference' => 'dd738d0b4491f32725492cf345f6b501f5922fec',
+    ),
+    'psr/log-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0.0',
+      ),
+    ),
+    'symfony/deprecation-contracts' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '2.3.x-dev',
+      ),
+      'reference' => '366d03137004f7fd0e9d9ae9360eaf2d5c66d1ad',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+        0 => '1.22.x-dev',
+      ),
+      'reference' => 'c6c942b1ac76c82448322025e084cadc56048b4e',
+    ),
+    'symfony/yaml' => 
+    array (
+      'pretty_version' => '5.x-dev',
+      'version' => '5.9999999.9999999.9999999-dev',
+      'aliases' => 
+      array (
+        0 => '9999999-dev',
+      ),
+      'reference' => '785f8b87649c51be5a756272080ec8bb52f5f914',
+    ),
+  ),
+);
