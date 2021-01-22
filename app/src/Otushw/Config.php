a6c64b37f26408a0dc9500f466fd17059fbf32aa
Alex <mar4ehk0@gmail.com>
2021-01-22 22:09:33 +0300

    hw4-3 added Config for getting variable from config.yaml

---
+++ app/src/Otushw/Config.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Otushw;
+
+use Exception;
+
+class Config
+{
+    private string $filePath;
+
+    public function __construct(string $filePath = '')
+    {
+        if (empty($filePath)) {
+            $filePath = self::FILE_NAME;
+        }
+
+        if (empty(file_exists($filePath))) {
+            throw new UserException($filePath . ' does not exit.');
+        }
+
+        $this->filePath = $filePath;
+    }
+
+    public function load(): void
+    {
+        foreach ($this->readFile() as $varName => $varValue) {
+            $_ENV[$varName] = $varValue;
+        }
+    }
+
+    /**
+     * @return array
+     * @throws Exception
+     */
+    private function readFile(): array
+    {
+        return \Symfony\Component\Yaml\Yaml::parseFile($this->filePath);
+    }
+
+}
