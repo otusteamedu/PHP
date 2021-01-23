64813fe57873831b33749e76da2d44477701d11c
Alex <mar4ehk0@gmail.com>
2021-01-23 11:23:50 +0300

    hw4-3 changed DBConnection

--- app/src/Otushw/Storage/DBConnection.php
+++ app/src/Otushw/Storage/DBConnection.php
@@ -9,9 +9,31 @@ use PDOException;
 
 class DBConnection
 {
-    private static $instance = null;
+    private static $pdo = null;
 
-    private function __construct() {
+    private function __construct() { }
+    private function __clone() { }
+    private function __wakeup() { }
+
+    /**
+     * @return PDO
+     * @throws AppException
+     */
+    public static function getInstance(): PDO
+    {
+        if (self::$pdo != null) {
+            return self::$pdo;
+        }
+        self::$pdo = self::connect();
+        return self::$pdo;
+    }
+
+    /**
+     * @return PDO
+     * @throws AppException
+     */
+    private function connect(): PDO
+    {
         $host = $_ENV['DB']['host'];
         $port = $_ENV['DB']['port'];
         $dbName = $_ENV['DB']['name'];
@@ -20,25 +42,16 @@ class DBConnection
 
         $dsn = "pgsql:host=$host;port=$port;dbname=$dbName;user=$userName;password=$password";
         try {
-            $this->instance = new PDO($dsn);
-            if(empty($this->instance)) {
+            $pdo = new PDO($dsn);
+            if(empty($pdo)) {
                 throw new AppException('DB connection is unsuccessful');
             }
+            return $pdo;
         } catch (PDOException $e) {
             throw new AppException($e->getMessage(), $e->getCode());
         }
     }
 
-    private function __clone() { }
-    private function __wakeup() { }
-
-    public static function getInstance()
-    {
-        if (self::$instance != null) {
-            return self::$instance;
-        }
-        return new self;
-    }
 
 
 }
