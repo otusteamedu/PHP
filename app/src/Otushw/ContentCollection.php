f4a063489b5b7a0b334a48f81d85ce077f33e877
Alex <mar4ehk0@gmail.com>
2021-01-24 11:44:17 +0300

    hw4-3 added class ContentCollection

---
+++ app/src/Otushw/ContentCollection.php
@@ -0,0 +1,128 @@
+<?php
+
+
+namespace Otushw;
+
+use Iterator;
+
+class ContentCollection implements Iterator
+{
+    private int $pointer = 0;
+    private int $total = 0;
+    private array $objects = [];
+    private array $raw = [];
+
+    /**
+     * ContentCollection constructor.
+     *
+     * @param array $raw
+     */
+    public function __construct(array $raw = [])
+    {
+        if (!empty($raw)) {
+            $this->raw = $raw;
+            $this->total = count($raw);
+        }
+    }
+
+    /**
+     * @param Content $content
+     */
+    public function add(Content $content): void
+    {
+        $this->objects[$this->total] = $content;
+        $this->total++;
+    }
+
+    /**
+     * @param int $num
+     *
+     * @return null|Content
+     */
+    private function getRow(int $num): ?Content
+    {
+        if ($num >= $this->total || $num < 0) {
+            return null;
+        }
+
+        if (isset($this->objects[$num])) {
+            return $this->objects[$num];
+        }
+
+        if (isset($this->raw[$num])) {
+            $this->objects[$num] = $this->createContentObject($this->raw[$num]);
+            return $this->objects[$num];
+        }
+
+        return null;
+    }
+
+    /**
+     * @param array $raw
+     *
+     * @return Content
+     */
+    private function createContentObject(array $raw): Content
+    {
+        $current = $this->getFromMap($raw['id']);
+        if (!is_null($current)) {
+            return $current;
+        }
+
+        return new Content(
+            $raw['id'],
+            $raw['name'],
+            $raw['id_genre'],
+            $raw['age_limit'],
+            $raw['move_lenght'],
+        );
+    }
+
+    /**
+     * @return Content
+     */
+    public function current(): ?Content
+    {
+        return $this->getRow($this->pointer);
+    }
+
+    /**
+     * @return Content
+     */
+    public function next(): ?Content
+    {
+        $this->pointer++;
+        return $this->current();
+    }
+
+    /**
+     * @return int
+     */
+    public function key(): int
+    {
+        return $this->pointer;
+    }
+
+    /**
+     * @return bool
+     */
+    public function valid(): bool
+    {
+        return (!is_null($this->current()));
+    }
+
+    public function rewind()
+    {
+        $this->pointer = 0;
+    }
+
+    /**
+     * @param int $id
+     *
+     * @return Content|null
+     */
+    private function getFromMap(int $id): ?Content
+    {
+        return ContentWatcher::getItem($id);
+    }
+}
