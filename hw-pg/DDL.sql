96ac77b1c161675e2239329752d49dffb9383d65
Sergey Selenskiy <szelenskiy@me.com>
2021-01-27 13:58:20 +0600

    random_string()

--- hw-pg/DDL.sql
+++ hw-pg/DDL.sql
@@ -1,3 +1,5 @@
+-- tables
+
 CREATE TABLE halls
 (
     id    smallserial NOT NULL,
@@ -86,3 +88,27 @@ CREATE TABLE films_attr_values
     CONSTRAINT films_attr_values_films_fk FOREIGN KEY (film_id) REFERENCES films (id),
     CONSTRAINT films_attr_values_attr_fk FOREIGN KEY (attr_id) REFERENCES films_attr (id)
 );
+
+-- functions
+
+CREATE
+OR REPLACE FUNCTION random_string(length integer) RETURNS text AS
+$$
+declare
+chars text[] := '{0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}';
+  result
+text := '';
+  i
+integer := 0;
+begin
+  if
+length < 0 then
+    raise exception 'Given length cannot be less than 0';
+end if;
+for i in 1..length loop
+    result := result || chars[1+random()*(array_length(chars, 1)-1)];
+end loop;
+return result;
+end;
+$$
+LANGUAGE plpgsql;
