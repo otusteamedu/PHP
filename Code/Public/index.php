131f75f13702da40606a2c2821f4fd354b69d7b5
Vladimir Nikitin <v.nikitin@squesito.ru>
2021-01-31 16:36:04 +0300

    hw1-2

---
+++ Code/Public/index.php
@@ -0,0 +1,3 @@
+<?php 
+
+echo "<h1>Vagrant Server OTUS PHP VNikitin - ".date("H:i:s d-m-Y")."</h1>";
--- Code/index.php
+++
@@ -1,3 +0,0 @@
-<?php 
-
-echo "<h1>Домашнее задание OTUS PHP (1-1) Docker Compose - ".date("H:i:s d-m-Y")."</h1>";
