541e0e4879f5061adbb24178cabfbdb657a8a919
Alex Dontsov <dcorpsoftware@gmail.com>
2021-02-01 15:23:02 +0700

    update ddl

--- hw3.1/create_ddl.sql
+++ hw3.1/create_ddl.sql
@@ -1,81 +1,93 @@
--- create database
+create sequence halls_id_seq;
 
-CREATE DATABASE cinema;
+alter sequence halls_id_seq owner to postgres;
 
--- users
-
--- public.users definition
-
--- Drop table
-
--- DROP TABLE public.users;
-
-CREATE TABLE public.users (
-  id serial NOT NULL,
-  username varchar(50) NOT NULL,
-  password varchar(50) NOT NULL,
-  email varchar(255) NOT NULL,
-  created_on timestamp NOT NULL,
-  last_login timestamp NULL,
-  CONSTRAINT users_email_key UNIQUE (email),
-  CONSTRAINT users_pkey PRIMARY KEY (id)
+create table if not exists users
+(
+    id serial not null
+        constraint users_pkey
+            primary key,
+    username varchar(50) not null,
+    password varchar(50) not null,
+    email varchar(255) not null
+        constraint users_email_key
+            unique,
+    created_on timestamp not null,
+    last_login timestamp
 );
 
+alter table users owner to postgres;
 
--- films
-
--- public.films definition
-
--- Drop table
-
--- DROP TABLE public.films;
-
-CREATE TABLE public.films (
-   id int4 NOT NULL,
-   name varchar(500) NOT NULL DEFAULT ''::character varying,
-   cost_rental float4 NOT NULL DEFAULT 0,
-   views_num int4 NOT NULL DEFAULT 0,
-   hall_id int4 NOT NULL,
-   started_at timestamp NULL,
-   finished_at timestamp NULL,
-   CONSTRAINT films_pk PRIMARY KEY (id)
+create table if not exists halls
+(
+    id integer not null
+        constraint halls_pk
+            primary key,
+    capacity integer not null
+        constraint capacity
+            check ((capacity > 0) AND (capacity <= 1000)),
+    number integer
 );
 
+alter table halls owner to postgres;
 
--- public.films foreign keys
-
-ALTER TABLE public.films ADD CONSTRAINT films_halls_id_fk FOREIGN KEY (hall_id) REFERENCES halls(id);
-
--- halls
-
--- public.halls definition
-
--- Drop table
-
--- DROP TABLE public.halls;
-
-CREATE TABLE public.halls (
-  id int4 NOT NULL,
-  capacity int4 NOT NULL,
-  CONSTRAINT halls_pk PRIMARY KEY (id)
+create table if not exists places
+(
+    id serial not null
+        constraint places_pk
+            primary key,
+    row integer default 0 not null,
+    "column" integer default 0 not null,
+    hall_id integer
 );
 
--- films_users
+alter table places owner to postgres;
 
--- public.films_users definition
-
--- Drop table
-
--- DROP TABLE public.films_users;
-
-CREATE TABLE public.films_users (
-    film_id int4 NOT NULL,
-    user_id int4 NOT NULL,
-    CONSTRAINT films_users_pk PRIMARY KEY (film_id, user_id)
+create table if not exists films
+(
+    id serial not null
+        constraint films_pk
+            primary key,
+    name varchar,
+    year integer not null,
+    country varchar
 );
 
+alter table films owner to postgres;
 
--- public.films_users foreign keys
+create table if not exists films_schedule
+(
+    id serial not null
+        constraint films_schedule_pk
+            primary key,
+    price real default 0 not null,
+    hall_id integer not null
+        constraint films_halls_id_fk
+            references halls,
+    started_at timestamp,
+    finished_at timestamp,
+    film_id integer not null
+        constraint films_schedule_films_id_fk
+            references films
+);
 
-ALTER TABLE public.films_users ADD CONSTRAINT films_users_films_id_fk FOREIGN KEY (film_id) REFERENCES films(id);
-ALTER TABLE public.films_users ADD CONSTRAINT films_users_users_id_fk FOREIGN KEY (user_id) REFERENCES users(id);
+alter table films_schedule owner to postgres;
+
+create table if not exists tickets
+(
+    place_id integer not null
+        constraint tickets_places_id_fk
+            references places,
+    user_id integer
+        constraint tickets_users_id_fk
+            references users,
+    film_schedule_id integer
+        constraint tickets_films_schedule_id_fk
+            references films_schedule,
+    id serial not null
+        constraint tickets_pk
+            primary key
+);
+
+alter table tickets owner to postgres;
+
