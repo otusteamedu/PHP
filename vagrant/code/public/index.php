a8e14d17943d7ff629356e93a52de9f0b0475d74
KleymenovN <kleymenov68@yandex.ru>
2021-02-04 09:29:57 +0300

    removed all unnecessary from vagrant

--- vagrant/code/public/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-echo "Hello from Vagrant!"; 
-echo "\n";
--- vagrant/scripts/configure-avahi.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sed -i "s/#browse-domains=.*/browse-domains=$1/" /etc/avahi/avahi-daemon.conf
-sed -i "s/browse-domains=.*/browse-domains=$1/" /etc/avahi/avahi-daemon.conf
-service avahi-daemon restart
