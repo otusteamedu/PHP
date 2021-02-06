601697c43776367f70caaac17dc963f35ed73cd0
ghostlyit <supermaximal23@gmail.com>
2021-02-07 01:26:22 +0300

    bash script for addition 2 numbers

---
+++ hw2/hw2-1/addition.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+regEx="^[+-]?[0-9]+$" # Регулярное выражение для проверки типа входных данных
+echo "Скрипт для сложения двух чисел."
+
+echo "Введите первое число:"
+read firstNumber
+
+echo "Введите второе число:"
+read secondNumber
+
+if ! [[ $firstNumber =~ $regEx ]] || ! [[ $secondNumber =~ $regEx ]]
+then
+echo "Введенные значения не являются целыми числами"
+exit 1
+fi
+
+sum=$(($firstNumber + $secondNumber))
+echo "Сумма: $sum"
+exit 0
