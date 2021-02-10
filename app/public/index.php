5de07febd76ebe642b4e3ed582c125db752c77f3
yessaliyev <40271311+yessaliyev@users.noreply.github.com>
2021-02-10 22:18:33 +0600

    first commit

--- app/public/index.php
+++ app/public/index.php
@@ -1,11 +1,45 @@
 <?php
 require_once '../bootstrap/bootstrap.php';
 
-use VideoPlatform\App;
-
 try {
-    $app = new App();
-    $app->run();
+
+    $redis = new Redis();
+    var_dump($redis->connect('redis', 6379));
+
+    $test = [
+        "priority" => 1000,
+        "params" => [
+            'param1' => 2,
+            'param2' => 2
+        ],
+        "event" => [
+            'event' => 'some_event'
+        ]
+    ];
+
+    $test2 = [
+        "priority" => 1001,
+        "params" => [
+            'param1' => 1,
+            'param2' => 2
+        ],
+        "event" => [
+            'event' => 'some_event'
+        ]
+    ];
+    $redis->zAdd('key', $test['priority'], json_encode($test['params']));
+    $redis->zAdd('key', $test2['priority'], json_encode($test2['params']));
+
+    print_r($redis->zPopMax('key',1));
+    print_r($redis->zPopMax('key',1));
+    print_r($redis->zPopMax('key',1));
+//        $redis->zRange('key', 0, -1);
+//    print_r($redis->zRevRange('key', 0, -1, true));
+//    $redis->zRemRangeByScore('key',0,1500);
+//    var_dump($redis->zRevRange('key', 0, -1, true));
+    $redis->close();
+//    $app = new App();
+//    $app->run();
 } catch (\Exception $e) {
     echo $e->getMessage();
 }
