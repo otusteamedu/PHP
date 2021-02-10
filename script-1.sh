f211d5a175d4713200c7d1ba6f0907dde6f433d2
KleymenovN <kleymenov68@yandex.ru>
2021-02-10 19:10:28 +0300

    last fix for first script i guess

--- script-1.sh
+++ script-1.sh
@@ -9,8 +9,8 @@ check1=`echo "$param1" | grep -E "$re"`
 check2=`echo "$param2" | grep -E "$re"`
 
 if [ "$check1" != '' ] && [ "$check2" != '' ]; then    
-  res=`echo "$param1 + $param2" | bc`
-  echo "Результат: ${res}"
+  echo "Результат:"  
+  awk 'BEGIN {print ARGV[1] + ARGV[2]}' "$param1" "$param2"
 else
   echo "Можно складывать только числа!"
   exit 1
