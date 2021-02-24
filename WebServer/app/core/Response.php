c2a4b85a71fba546a8e5ab0ffda3630b286aa285
konstantin <noskov.kos@gmail.com>
2021-02-24 22:04:33 +0300

    Create validate string app

---
+++ WebServer/app/core/Response.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace Core;
+
+
+class Response
+{
+    private string $controller;
+    private string $action;
+    private array $arguments;
+
+    public function __construct(string $controller, string $action, array $arguments = [])
+    {
+        $this->action = $action;
+        $this->controller = $controller;
+        $this->arguments = $arguments;
+    }
+
+    public function __invoke(): ?string
+    {
+        if(false === class_exists($this->controller)){
+            throw new \RuntimeException('Controller ' . $this->controller . ' not exist');
+        }
+
+        $controllerObj = new $this->controller;
+
+        return call_user_func_array([$controllerObj, $this->action], $this->arguments);
+    }
+}
