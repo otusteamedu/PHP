adf7ae1962a11eead1dac2593fdf75f1dca3ab58
Anatoly Kochetkov <yolar.sh@gmail.com>
2021-03-01 21:14:16 +0400

    migrate to postgresql

--- cinema.sql
+++ cinema.sql
@@ -1,36 +1,47 @@
-CREATE TABLE `hall` (
-    `id` tinyint(4) unsigned NOT NULL AUTO_INCREMENT,
-    `name` varchar(32) NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `uidx_name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE "public"."hall" (
+    "id" SERIAL NOT NULL,
+    "name" CHARACTER VARYING (32) NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NULL,
+    CONSTRAINT "hall_pkey" PRIMARY KEY ("id")
+);
 
-CREATE TABLE `movie` (
-    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-    `name` varchar(120) NOT NULL,
-    `description` varchar(320) NOT NULL,
-    `duration` tinyint(4) unsigned NOT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `uidx_name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE UNIQUE INDEX "uidx_hall_name" ON "public"."hall" ("name");
 
-CREATE TABLE `showtime` (
-    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-    `hall_id` tinyint(4) unsigned NOT NULL,
-    `movie_id` int(11) unsigned NOT NULL,
-    `starts_in` datetime NOT NULL,
-    PRIMARY KEY (`id`),
-    KEY `fk_showtime_hall_id` (`hall_id`),
-    KEY `fk_showtime_movie_id` (`movie_id`),
-    CONSTRAINT `fk_showtime_hall_id` FOREIGN KEY (`hall_id`) REFERENCES `hall` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-    CONSTRAINT `fk_showtime_movie_id` FOREIGN KEY (`movie_id`) REFERENCES `movie` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE "public"."movie" (
+    "id" SERIAL NOT NULL,
+    "name" CHARACTER VARYING (128) NOT NULL,
+    "description" CHARACTER VARYING (320) NULL,
+    "duration" INTEGER NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NULL,
+    CONSTRAINT "movie_pkey" PRIMARY KEY ("id")
+);
 
-CREATE TABLE `ticket` (
-    `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-    `showtime_id` int(11) unsigned NOT NULL,
-    `price` decimal(8,2) NOT NULL,
-    PRIMARY KEY (`id`),
-    KEY `fk_ticket_showtime_id` (`showtime_id`),
-    CONSTRAINT `fk_ticket_showtime_id` FOREIGN KEY (`showtime_id`) REFERENCES `showtime` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE UNIQUE INDEX "uidx_movie_name" ON "public"."movie" ("name");
+
+CREATE TABLE "public"."showtime" (
+    "id" SERIAL NOT NULL,
+    "hall_id" INTEGER NOT NULL,
+    "movie_id" INTEGER NOT NULL,
+    "starts_in" TIMESTAMP NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NULL,
+    CONSTRAINT "showtime_pkey" PRIMARY KEY ("id")
+);
+
+ALTER TABLE "public"."showtime"
+    ADD CONSTRAINT "fk_showtime_hall_id" FOREIGN KEY ("hall_id") REFERENCES "public"."hall" ("id") ON DELETE CASCADE ON UPDATE NO ACTION,
+    ADD  CONSTRAINT "fk_showtime_movie_id" FOREIGN KEY ("movie_id") REFERENCES "public"."movie" ("id") ON DELETE CASCADE ON UPDATE NO ACTION;
+
+CREATE TABLE "public"."ticket" (
+    "id" SERIAL NOT NULL,
+    "showtime_id" INTEGER NOT NULL,
+    "price" DECIMAL (8, 2) NOT NULL,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NULL,
+    CONSTRAINT "ticket_pkey" PRIMARY KEY ("id")
+);
+
+ALTER TABLE "public"."ticket"
+    ADD CONSTRAINT "fk_ticket_showtime_id" FOREIGN KEY ("showtime_id") REFERENCES "public"."showtime" ("id") ON DELETE CASCADE ON UPDATE NO ACTION;
