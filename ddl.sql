138001fbed53dcb79718f90950417caabf715763
KleymenovN <kleymenov68@yandex.ru>
2021-03-05 11:26:46 +0300

    schema fixed

--- ddl.sql
+++ ddl.sql
@@ -35,24 +35,44 @@ CREATE INDEX movie_id_idx ON public.movie USING btree (id);
 
 -- DROP TABLE public."session";
 
+CREATE TABLE public."type" (
+	id int2 NOT NULL GENERATED ALWAYS AS IDENTITY,
+	"name" varchar NOT NULL,
+	price money NOT NULL,
+	CONSTRAINT type_pk PRIMARY KEY (id);
+);
+
+
 CREATE TABLE public."session" (
 	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
 	movie_id int4 NOT NULL,
 	"date" timestamptz(0) NOT NULL,
 	CONSTRAINT session_pk PRIMARY KEY (id),
-	CONSTRAINT session_fk FOREIGN KEY (movie_id) REFERENCES movie(id) ON UPDATE CASCADE ON DELETE CASCADE
+	CONSTRAINT session_fk FOREIGN KEY (movie_id) REFERENCES movie(id) ON UPDATE CASCADE ON DELETE CASCADE;
 );
 -- Drop table
 
 -- DROP TABLE public.ticket;
 
-CREATE TABLE public.ticket (
+CREATE TABLE public.session_location (
+	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
 	session_id int4 NOT NULL,
 	location_id int4 NOT NULL,
-	is_bought int2 NOT NULL DEFAULT 0,
+	is_free int2 NOT NULL DEFAULT 1,
+	type_id int2 NOT NULL,
+	CONSTRAINT session_location_pk PRIMARY KEY (id),
+	CONSTRAINT session_location_fk FOREIGN KEY (location_id) REFERENCES public."location"(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT session_location_fk_1 FOREIGN KEY (session_id) REFERENCES public."session"(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CONSTRAINT session_location_fk FOREIGN KEY (type_id) REFERENCES public."type"(id) ON DELETE CASCADE ON UPDATE CASCADE;
+);
+
+
+CREATE TABLE public.ticket (
+	id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
+	session_location_id int4 NOT NULL,
 	price money NOT NULL,
-	CONSTRAINT ticket_pk PRIMARY KEY (session_id, location_id),
-	CONSTRAINT ticket_fk FOREIGN KEY (session_id) REFERENCES session(id),
-	CONSTRAINT ticket_fk_1 FOREIGN KEY (location_id) REFERENCES location(id)
+	CONSTRAINT ticket_pk PRIMARY KEY (id),
+	CONSTRAINT ticket_fk FOREIGN KEY (session_location_id) REFERENCES public.session_location(id) ON DELETE CASCADE ON UPDATE CASCADE,
+	CREATE INDEX ticket_session_location_id_idx ON public.ticket (session_location_id);
 );
 
