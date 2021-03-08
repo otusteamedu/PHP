53d6d1876b8eddee5d1149d5fd829271238b550d
konstantin <noskov.kos@gmail.com>
2021-03-08 15:12:30 +0300

    Create cinema theater`s db project.

---
+++ ProjectDB/sqls/select_the_most_profitable_film.sql
@@ -0,0 +1,6 @@
+SELECT f.name, COUNT (t.id) as tickets_count_purchase, SUM(s.price) as yield FROM public.films AS f
+JOIN public.seances AS s ON f.id = s.film_id
+JOIN public.tickets AS t ON s.id = t.seance_id
+GROUP BY f.id
+ORDER BY yield DESC
+LIMIT 1
