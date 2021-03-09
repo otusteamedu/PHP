d346bb63dbc6e436d0825b08693ac09d38dd99dc
konstantin <noskov.kos@gmail.com>
2021-03-09 22:32:08 +0300

    Fixed some remarks

--- ProjectDB/sqls/select_the_most_profitable_film.sql
+++ ProjectDB/sqls/select_the_most_profitable_film.sql
@@ -1,4 +1,4 @@
-SELECT f.name, COUNT (t.id) as tickets_count_purchase, SUM(s.price) as yield FROM public.films AS f
+SELECT f.name, COUNT (t.id) as tickets_count_purchase, SUM(t.price) as yield FROM public.films AS f
 JOIN public.seances AS s ON f.id = s.film_id
 JOIN public.tickets AS t ON s.id = t.seance_id
 GROUP BY f.id
