497a9de1c802b791e3f062667948809ad0d48b19
Dmitriy Korytnyi <dmitriy.korytnyi@optimusite.com>
2021-03-10 23:07:07 +0200

    hw4 Brackets script

--- hw4/code/index.php
+++ hw4/code/index.php
@@ -1,4 +1,34 @@
 <?php
-echo "Homework 3-3";
 
-phpinfo();
+// POST processing
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if(empty($_POST['string'])) {
+        $error = 'Строка не может быть пустой';
+    } elseif(strlen($_POST['string']) < 2 || strlen($_POST['string']) > 256) {
+        $error = 'Не корректная длина строки';
+    } elseif (strlen(str_replace(')', '', str_replace('(', '', $_POST['string']))) > 0) {
+        $error = 'Строка содержит символы, отличные от скобкок';
+    } else {
+        // Check bracket sequence
+        $bracketCount = 0;
+        for($i = 0; $i < strlen($_POST['string']); $i++) {
+            if($_POST['string'][$i] === '(') {
+                $bracketCount++;
+            } else {
+                $bracketCount--;
+            }
+        }
+        if($bracketCount !== 0) {
+            $error = 'Не верное количество открытых и закрытых скобок';
+        } else {
+            $success = 'Количество открытых и закрытых скобок совпадает';
+        }
+    }
+}
+
+if(isset($error)) {
+    http_response_code(400);
+}
+
+// Render the page
+include_once('client.php');
