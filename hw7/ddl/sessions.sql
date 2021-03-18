62ac97e1f37c923c78eb8ea54a0a0932b8b8ef33
kaivladimirv <kaivladimirv@gmail.com>
2021-03-18 13:44:50 +0600

    hw7(fix): Объединить поля start_date и start_time

--- hw7/ddl/sessions.sql
+++ hw7/ddl/sessions.sql
@@ -1,12 +1,11 @@
 create table sessions
 (
     id bigint not null primary key,
-    start_date date not null,
-    start_time time not null,
+    start_date timestamp not null,
     hall_id bigint not null
         references halls on update cascade,
     film_id bigint not null
         references films on update cascade,
     price integer not null,
-    unique (start_date, start_time, hall_id)
+    unique (start_date, hall_id)
 );
