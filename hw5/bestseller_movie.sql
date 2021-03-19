65c247f87fd66a518f7a4076e4d5e52e0beec2e7
aivanov <a3bg5@protonmail.com>
2021-03-19 18:33:59 +0200

    add fix when ticket_price is null

--- hw5/bestseller_movie.sql
+++ hw5/bestseller_movie.sql
@@ -3,5 +3,6 @@ from movies m
          left join sessions s on s.movie_id = m.id
          left join tickets t on t.session_id = s.id
          left join prices p on p.ticket_id = t.id
+where ticket_price is not null
 group by m.id
 order by movie_income desc limit 1;
