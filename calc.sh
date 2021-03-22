b1aba43838bc0e1059913bb8ed8d403caa716e2d
Сергей Диденко <s.didenko@rabota.ru>
2021-03-22 23:48:13 +0300

    заменил bc на awk

--- calc.sh
+++ calc.sh
@@ -5,7 +5,7 @@ checkFloat='^[-0-9]+\.[0-9]+$'
 
 if [[ $1 =~ $checkNum ||  $1 =~ $checkFloat ]] && [[ $2 =~ $checkNum  ||  $2 =~ $checkFloat ]]
 then
-  echo "$1 + $2" | bc -l
+   awk "BEGIN { print($1 + $2) }"
 else
     echo "Один из аргументов не является числом"
 fi
