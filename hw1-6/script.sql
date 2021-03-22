1d0a37ef7e679c79a1903f91112be4252cf61f1a
aic513 <aic513@mail.ru>
2021-03-22 15:10:56 +0300

    hw1-6: add place price

--- hw1-6/script.sql
+++ hw1-6/script.sql
@@ -4,8 +4,10 @@ DROP TABLE IF EXISTS `order`;
 DROP TABLE IF EXISTS ticket;
 DROP TABLE IF EXISTS hall;
 DROP TABLE IF EXISTS place;
+DROP TABLE IF EXISTS place_category;
 DROP TABLE IF EXISTS customer;
 DROP TABLE IF EXISTS film_schedule;
+DROP TABLE IF EXISTS film_schedule_place_price;
 DROP TABLE IF EXISTS film;
 
 CREATE TABLE if not exists booking
@@ -15,7 +17,7 @@ CREATE TABLE if not exists booking
     employee_id integer REFERENCES employee (id),
     created_at  datetime,
     updated_at  datetime
-);
+    );
 
 CREATE TABLE if not exists employee
 (
@@ -23,7 +25,7 @@ CREATE TABLE if not exists employee
     name            VARCHAR(15),
     position        VARCHAR(15),
     start_work_date date
-);
+    );
 
 CREATE TABLE if not exists `order`
 (
@@ -31,7 +33,7 @@ CREATE TABLE if not exists `order`
     ticket_id  integer REFERENCES ticket (id),
     created_at datetime,
     updated_at datetime
-);
+    );
 
 CREATE TABLE if not exists ticket
 (
@@ -40,23 +42,29 @@ CREATE TABLE if not exists ticket
     film_schedule_id integer REFERENCES film_schedule (id),
     customer_id      integer REFERENCES customer (id),
     date             date
-);
+    );
 
 CREATE TABLE if not exists hall
 (
     id           SERIAL PRIMARY KEY,
     name         VARCHAR(15),
     limit_places integer
-);
+    );
 
 CREATE TABLE if not exists place
 (
-    id      SERIAL PRIMARY KEY,
-    row     integer,
-    col     integer,
-    hall_id integer REFERENCES hall (id)
+    id                SERIAL PRIMARY KEY,
+    row               integer,
+    col               integer,
+    hall_id           integer REFERENCES hall (id),
+    place_category_id integer REFERENCES place_category (id)
+    );
 
-);
+CREATE TABLE if not exists place_category
+(
+    id       SERIAL PRIMARY KEY,
+    category VARCHAR(25)
+    );
 
 CREATE TABLE if not exists customer
 (
@@ -67,7 +75,7 @@ CREATE TABLE if not exists customer
     login      varchar(10),
     created_at datetime,
     updated_at datetime
-);
+    );
 
 CREATE TABLE if not exists film
 (
@@ -76,27 +84,33 @@ CREATE TABLE if not exists film
     year        varchar(10),
     country     varchar(127),
     description varchar(127)
-
-);
+    );
 
 CREATE TABLE if not exists film_schedule
 (
     id          serial primary key,
     film_id     integer REFERENCES film (id),
-    price       numeric(10, 2),
     start_at    time,
     finished_at time
-);
+    );
 
-#most commercial film
-SELECT film.name,
-       earnings.money
+CREATE TABLE if not exists film_schedule_place_price
+(
+    id                serial primary key,
+    film_schedule_id  integer REFERENCES film_schedule (id),
+    place_category_id integer REFERENCES place_category (id),
+    price             numeric(10, 2)
+    );
+
+SELECT film.name, earnings.money
 FROM film
-         LEFT JOIN (SELECT film_schedule.film_id,
-                           SUM(film_schedule.price) AS money
+         LEFT JOIN (SELECT film_schedule.film_id, SUM(fspp.price) as money
                     FROM ticket
                              LEFT JOIN film_schedule ON film_schedule.id = ticket.film_schedule_id
+                             LEFT JOIN place p ON ticket.place_id = p.id
+                             LEFT JOIN film_schedule_place_price AS fspp ON film_schedule.id = fspp.film_schedule_id
+                    WHERE fspp.price IS NOT NULL
                     GROUP BY film_schedule.film_id) AS earnings
                    ON film.id = earnings.film_id
 ORDER BY earnings.money DESC
-LIMIT 1;
+    LIMIT 1;
