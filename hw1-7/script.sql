bf2355f3e1525bbbb8533ae79fd86e68209e908f
aic513 <aic513@mail.ru>
2021-03-26 15:04:55 +0300

    hw1-7: change types

--- hw1-7/script.sql
+++ hw1-7/script.sql
@@ -21,7 +21,7 @@ CREATE TABLE if not exists filmsAttr
     id      serial primary key,
     attr    varchar(128),
     type_id integer,
-    CONSTRAINT fk_type_id FOREIGN KEY (type_id) REFERENCES filmsAttrTypes (id)
+    CONSTRAINT fk_type_id FOREIGN KEY (type_id) REFERENCES filmsAttrTypes (id) ON DELETE CASCADE
 );
 
 CREATE TABLE if not exists filmsAttrValues
@@ -33,9 +33,9 @@ CREATE TABLE if not exists filmsAttrValues
     value_date  date,
     value_bool  boolean,
     description varchar(127),
-    value_price float4,
-    CONSTRAINT fk_film_id FOREIGN KEY (film_id) REFERENCES films (id),
-    CONSTRAINT fk_attr_id FOREIGN KEY (attr_id) REFERENCES filmsAttr (id)
+    value_float float4,
+    CONSTRAINT fk_film_id FOREIGN KEY (film_id) REFERENCES films (id) ON DELETE CASCADE,
+    CONSTRAINT fk_attr_id FOREIGN KEY (attr_id) REFERENCES filmsAttr (id) ON DELETE CASCADE
 
 );
 
