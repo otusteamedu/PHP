b6f9e646bf96b9a242003fef2dd951b24d2db529
Alex <mar4ehk0@gmail.com>
2021-03-27 00:01:06 +0300

    hw9-2 clearned App

--- app/src/Otushw/App.php
+++ app/src/Otushw/App.php
@@ -2,25 +2,16 @@
 
 namespace Otushw;
 
-use GuzzleHttp\Psr7\ServerRequest;
-use Otushw\Storage\DBConnection;
-use Otushw\Storage\OrderMapper;
-use PDO;
-
 class App
 {
 
     private AppInstanceAbstract $appInstance;
-    private PDO $pdo;
-    private OrderMapper $orderMapper;
 
     public function __construct()
     {
         $this->loadConfig();
         $app = new AppFactory();
         $this->appInstance = $app->create();
-//        $this->pdo = DBConnection::getInstance();
-//        $this->orderMapper = new OrderMapper($this->pdo);
     }
 
     private function loadConfig(): void
