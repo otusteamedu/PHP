51dce409bd0138b84b20643eba828846c432186a
Alex <mar4ehk0@gmail.com>
2021-03-27 00:00:50 +0300

    hw9-2 implemented RabbitMQConsumer

--- app/src/Otushw/Queue/RabbitMQ/RabbitMQConsumer.php
+++ app/src/Otushw/Queue/RabbitMQ/RabbitMQConsumer.php
@@ -3,14 +3,17 @@
 
 namespace Otushw\Queue\RabbitMQ;
 
+use Otushw\Logger\AppLogger;
 use Otushw\Queue\QueueConsumerInterface;
-use Otushw\Message;
+use Otushw\ServerQueue\Jobs\Worker;
 
 class RabbitMQConsumer extends RabbitMQ implements QueueConsumerInterface
 {
 
     public function consume(): void
     {
+        AppLogger::addInfo('RabbitMQ:Consumer was ran');
+
         $this->channel->basic_qos(null, 1, null);
         $this->channel->basic_consume(
             self::QUEUE_NAME,
@@ -29,11 +32,12 @@ class RabbitMQConsumer extends RabbitMQ implements QueueConsumerInterface
 
     public function processMessage($msg): void
     {
-        $data = json_decode($msg->body);
-        if (!empty($data)) {
-            foreach ($data as $item) {
-                Message::showMessage($item);
-            }
+        AppLogger::addInfo('RabbitMQ:Consumer received message', [$msg->body]);
+        $worker = new Worker($msg->body);
+        $job = $worker->create();
+        $job->do();
+        if ($job->isCompleted()) {
+            $worker->finish();
             $this->channel->basic_ack($msg->delivery_info['delivery_tag']);
         }
     }
