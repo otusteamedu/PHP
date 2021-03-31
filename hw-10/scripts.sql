d4e8e744dee1dfacd8267e30c09a36d408cc8caf
genamalugin <genamalugin@gmail.com>
2021-03-31 10:05:08 +0300

    6 database queries

--- hw-10/scripts.sql
+++ hw-10/scripts.sql
@@ -74,6 +74,8 @@ CREATE TABLE ticket
     order_id      BIGINT REFERENCES orders
 );
 
+-- TRUNCATE TABLE ticket;
+
 ----------------------------------------------------
 --------------------- Demo Data --------------------
 ----------------------------------------------------
@@ -89,16 +91,25 @@ INSERT INTO hall_movie(cinema_id, movie_id, date_time)
 SELECT k + 1, k ^ 1, now()
 FROM generate_series(0, 100) AS k;
 
-INSERT INTO movies (name, description, time, box_office)
-SELECT k + 1, k ^ 1, k % 2::numeric, k % 2::numeric
+INSERT INTO movies (name,
+                    description,
+                    time,
+                    box_office)
+SELECT k + 1,
+       k ^ 1,
+       k % 2::numeric,
+       k % 2::numeric
 FROM generate_series(0, 10000) AS k;
 
 INSERT INTO seats(hall_id, seat_row, seat_no)
 SELECT k + 1, k ^ 1, k % 2
 FROM generate_series(0, 100) AS k;
 
-INSERT INTO session(movie_id, hall_movie_id, begin_movie,
-                    final_movie, price)
+INSERT INTO session(movie_id,
+                    hall_movie_id,
+                    begin_movie,
+                    final_movie,
+                    price)
 SELECT trunc(random() * 10 + 1),
        k % 2 + 1,
        now(),
@@ -117,7 +128,7 @@ INSERT INTO ticket(movie_id,
                    seat_row,
                    session_id,
                    order_id)
-SELECT k + 1,
+SELECT floor(random() * (1000 - 100 + 1)) + 100,
        floor(random() * (100 - 10 + 1)) + 1,
        floor(random() * (1000 - 100 + 1)) + 100,
        k%1,
@@ -126,8 +137,58 @@ SELECT k + 1,
        k + 1
 FROM generate_series(1, 10000) as k;
 
-
 ----------------------------------------------------
 --------------------- 6 запросов -------------------
 ----------------------------------------------------
 
+-- Находим сумму всех билетов определённого фильма
+SELECT sum(movie_id)
+FROM ticket
+WHERE movie_id = 647;
+
+-- Средняя стоимость билета
+SELECT movie_id,
+       round(avg(price), 2)
+FROM ticket
+GROUP BY movie_id
+ORDER BY movie_id ;
+
+-- Средняя стоимость билета определённого фильма по сравнению с текущей стоимостью билета
+SELECT movie_id,
+       price,
+       avg(price) OVER (ORDER BY movie_id)
+FROM ticket;
+
+-- Находим название фильма и сумму, которую собрал этот фильм
+SELECT movie_id,
+       (SELECT name FROM movies m
+        WHERE m.id = movie_id),
+       sum(price) sum
+FROM session
+GROUP BY movie_id
+ORDER BY sum DESC;
+
+--Вычитаем аренду в размере 50% и смотрим прибыль для каждого зала
+CREATE VIEW hall_profit AS
+SELECT hall_id,
+       sum(price)
+FROM ticket
+GROUP BY hall_id
+ORDER BY hall_id;
+
+SELECT h.hall_id,
+       h.sum,
+       abs(h.sum * 0.5) profit
+FROM hall_profit h
+ORDER BY profit DESC;
+
+-- Отображаем зал, фильм, время начала и конца фильма. Задействуем 3 таблицы.
+SELECT hall_movie_id,
+       s.movie_id,
+       (SELECT name
+        FROM movies m
+        WHERE m.id = s.movie_id),
+       s.begin_movie,
+       s.final_movie
+FROM hall_movie
+         INNER JOIN session s on hall_movie.id = s.hall_movie_id
