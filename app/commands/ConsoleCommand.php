1e30dd9862de6206ebb03f1ac250b99b47c1b45b
yessaliyev <40271311+yessaliyev@users.noreply.github.com>
2021-03-31 09:34:02 +0600

    refactoring

--- app/commands/ConsoleCommand.php
+++ app/commands/ConsoleCommand.php
@@ -2,16 +2,12 @@
 
 namespace Commands;
 
-use hanneskod\classtools\Iterator\ClassIterator;
-use Otus\Consumer\RabbitMQConsumers\BasicConsumers\ConsumerA;
-use Otus\Consumer\RabbitMQConsumers\RabbitMQConsumer;
+use Otus\Consumer\RabbitMQConsumers\BasicConsumers\ConsumerFactory;
 use Otus\Exceptions\AppException;
-use Symfony\Component\Finder\Finder;
 
 class ConsoleCommand
 {
     const BASIC_START = 'basic_start';
-    const BACKGROUND_START = 'background_start';
 
     private array $argv;
 
@@ -20,15 +16,15 @@ class ConsoleCommand
         $this->argv = $argv;
     }
 
+    /**
+     * @throws AppException
+     */
     public function run()
     {
         if (!empty($this->argv[1])) {
             switch ($this->argv[1]) {
                 case self::BASIC_START:
                     $this->basicStart();
-                    break;
-                case self::BACKGROUND_START:
-                    $this->backgroundStart();
                     break;
                 default:
                     throw new AppException('undefined command');
@@ -38,25 +34,7 @@ class ConsoleCommand
 
     private function basicStart()
     {
-        $basicConsumer = new ConsumerA();
+        $basicConsumer = ConsumerFactory::createConsumer();
         $basicConsumer->start();
-    }
-
-    /**
-     * найдет всех Consumer-ов
-     * из RABBITMQ_BASIC_CONSUMERS_PATH
-     * и запустит в фоновом режиме
-     */
-    private function backgroundStart()
-    {
-        $finder = new Finder;
-        $iter = new ClassIterator($finder->in($_ENV['RABBITMQ_BASIC_CONSUMERS_PATH']));
-
-        foreach ($iter->getClassMap() as $classname => $splFileInfo) {
-            $consumerClass = '\\' . $classname;
-            $encoded = json_encode($consumerClass);
-
-            exec('php consumerStarter.php ' . $encoded . '>/dev/null 2>&1 &');
-        }
     }
 }
--- app/public/index.php
+++ app/public/index.php
@@ -3,11 +3,12 @@
 require_once '../bootstrap/bootstrap.php';
 
 use Otus\App;
+use Otus\Message\Message;
 use Otus\Response\JsonResponse;
 
 try {
     $app = new App();
     $app->run();
 } catch (\Exception $e) {
-    JsonResponse::respond($e->getMessage(), $e->getCode());
+    Message::showMessage(JsonResponse::respond(['message' => $e->getMessage()],$e->getCode()));
 }
