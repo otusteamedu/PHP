6510a347c1a7e4b9ed39d37d89c60d7223c9c169
NightWizard75 <vladimir@valyakin.ru>
2021-04-02 16:21:16 +0300

    Add some indexes

--- db/postgres/createTables/EAV.sql
+++ db/postgres/createTables/EAV.sql
@@ -11,14 +11,17 @@ CREATE TABLE IF NOT EXISTS "attribute_type_prop" (
   attribute_type_id int2 references attribute_type(id)
 );
 CREATE UNIQUE INDEX attribute_type_prop_id_UNIQUE ON attribute_type_prop(id);
+CREATE INDEX attribute_type_prop_attribute_type_id_idx ON attribute_type_prop(attribute_type_id);
 
 CREATE TABLE IF NOT EXISTS "attribute" (
   id smallserial not null primary key,
   name VARCHAR(125) null,
+  showname VARCHAR(125) null,
   attribute_type_id int2 references attribute_type(id) 
   );
-comment on column "attribute".name is 'Наименование атрибута';
+comment on column "attribute".name is 'пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ';
 CREATE UNIQUE INDEX attribute_id_UNIQUE ON "attribute"(id);
+CREATE INDEX attribute_attribute_type_id_idx ON "attribute"(attribute_type_id);
 
 CREATE TABLE IF NOT EXISTS "attribute_value" (
   id smallserial not null primary key,
@@ -31,3 +34,6 @@ CREATE TABLE IF NOT EXISTS "attribute_value" (
   movie_id int4 references movie(id) 
 );
 CREATE UNIQUE INDEX attribute_value_id_UNIQUE ON attribute_value(id);
+CREATE INDEX attribute_value_attribute_id_idx ON attribute_value(attribute_id);
+CREATE INDEX attribute_value_movie_id_idx ON attribute_value(movie_id);
+
