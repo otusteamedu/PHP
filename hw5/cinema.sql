f85e0a5a5481eefa65723b548795f7684fba7f2a
Alyona <alyonacherny@gmail.com>
2021-04-04 18:17:07 +0500

    correction

--- hw5/cinema.sql
+++ hw5/cinema.sql
@@ -1,115 +1,127 @@
+-- DROP SCHEMA public;
 
--- База данных: `cinema`
+CREATE SCHEMA public AUTHORIZATION postgres;
 
--- Структура таблицы `clients`
+-- DROP SEQUENCE public.clients_id_seq;
 
-CREATE TABLE `clients` (
-  `id` int(133) NOT NULL AUTO_INCREMENT,
-  `surname` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `patronymic` varchar(255) NOT NULL,
-  `birthday` date NOT NULL,
-  `phone` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE SEQUENCE public.clients_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.films_id_seq;
 
-ALTER TABLE `clients`
-  ADD PRIMARY KEY (`id`);
+CREATE SEQUENCE public.films_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.halls_id_seq;
 
--- Дамп данных таблицы `clients`
+CREATE SEQUENCE public.halls_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.sessions_id_seq;
 
-INSERT INTO `clients` (`id`, `surname`, `name`, `patronymic`, `birthday`, `phone`) VALUES
-(1, 'Сидоров', 'Иван', 'Петрович', '1995-10-06', '8-922-111-11-11'),
-(2, 'Черняева', 'Елена', 'Васильевна', '1990-05-18', '8-922-111-11-12'),
-(3, 'Фамилия', 'Имя', 'Отчество', '1990-03-17', '8-922-111-11-14'),
-(4, 'Фамилия1', 'Имя1', 'Отчество1', '1990-03-17', '8-922-111-11-15'),
-(5, 'Фамилия2', 'Имя2', 'Отчество2', '1990-03-17', '8-922-111-11-16'),
-(6, 'Фамилия3', 'Имя3', 'Отчество3', '1990-03-17', '8-922-111-11-17');
+CREATE SEQUENCE public.sessions_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+-- DROP SEQUENCE public.tickets_id_seq;
 
--- Структура таблицы `films`
+CREATE SEQUENCE public.tickets_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;-- public.clients definition
 
-CREATE TABLE `films` (
-  `id` int(133) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `price` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Drop table
 
-ALTER TABLE `films`
-  ADD PRIMARY KEY (`id`);
+-- DROP TABLE public.clients;
 
--- Дамп данных таблицы `films`
+CREATE TABLE public.clients (
+	id bigserial NOT NULL,
+	surname varchar NULL,
+	"name" varchar NULL,
+	patronymic varchar NULL,
+	birthday date NULL,
+	phone varchar NULL,
+	CONSTRAINT clients_pkey PRIMARY KEY (id)
+);
 
-INSERT INTO `films` (`id`, `name`, `price`) VALUES
-(1, 'Простоквашино', 100),
-(2, 'Серый волк', 150),
-(3, 'Три богатыря', 200),
-(4, 'Мадагаскар', 200);
 
--- Структура таблицы `halls`
+-- public.films definition
 
-CREATE TABLE `halls` (
-  `id` int(133) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `capacity` int(133) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Drop table
 
--- Дамп данных таблицы `halls`
+-- DROP TABLE public.films;
 
-INSERT INTO `halls` (`id`, `name`, `capacity`) VALUES
-(1, 'Зал 1', 5),
-(2, 'Зал 2', 10);
+CREATE TABLE public.films (
+	id bigserial NOT NULL,
+	"name" varchar NULL,
+	duration time NULL,
+	CONSTRAINT films_pkey PRIMARY KEY (id)
+);
 
-ALTER TABLE `halls`
-  ADD PRIMARY KEY (`id`);
 
--- Структура таблицы `orders`
+-- public.halls definition
 
-CREATE TABLE `orders` (
-  `id` int(133) NOT NULL AUTO_INCREMENT,
-  `id_client` int(133) NOT NULL,
-  `id_session` int(133) NOT NULL,
-  `count` int(133) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Drop table
 
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`);
+-- DROP TABLE public.halls;
 
--- Дамп данных таблицы `orders`
+CREATE TABLE public.halls (
+	id serial NOT NULL,
+	"name" varchar NULL,
+	max_plase int4 NULL,
+	CONSTRAINT halls_pkey PRIMARY KEY (id)
+);
 
-INSERT INTO `orders` (`id`, `id_client`, `id_session`, `count`) VALUES
-(1, 1, 2, 1),
-(2, 3, 3, 3),
-(3, 4, 5, 2),
-(4, 5, 5, 4),
-(5, 2, 4, 4),
-(6, 5, 6, 5),
-(7, 6, 7, 2),
-(8, 1, 10, 5),
-(9, 3, 4, 1),
-(10, 5, 6, 4);
 
--- Структура таблицы `sessions`
+-- public.sessions definition
 
-CREATE TABLE `sessions` (
-  `id` int(133) NOT NULL AUTO_INCREMENT,
-  `id_hall` int(133) NOT NULL,
-  `id_film` int(133) NOT NULL,
-  `time` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- Drop table
 
-ALTER TABLE `sessions`
-  ADD PRIMARY KEY (`id`);
+-- DROP TABLE public.sessions;
 
--- Дамп данных таблицы `sessions`
+CREATE TABLE public.sessions (
+	id bigserial NOT NULL,
+	id_hall int4 NULL,
+	id_film int4 NULL,
+	"time" time NULL,
+	base_price float4 NULL,
+	CONSTRAINT sessions_pkey PRIMARY KEY (id),
+	CONSTRAINT sessions_fk FOREIGN KEY (id_film) REFERENCES films(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT sessions_fk_1 FOREIGN KEY (id_hall) REFERENCES halls(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
 
-INSERT INTO `sessions` (`id`, `id_hall`, `id_film`, `time`) VALUES
-(1, 1, 4, '08:00:00'),
-(2, 1, 1, '10:00:00'),
-(3, 1, 2, '12:00:00'),
-(4, 1, 4, '14:00:00'),
-(5, 2, 1, '14:00:00'),
-(6, 1, 2, '16:00:00'),
-(7, 2, 3, '16:00:00'),
-(8, 1, 2, '18:00:00'),
-(9, 2, 1, '18:00:00'),
-(10, 2, 3, '20:00:00');
 
+-- public.tickets definition
+
+-- Drop table
+
+-- DROP TABLE public.tickets;
+
+CREATE TABLE public.tickets (
+	id bigserial NOT NULL,
+	id_client int4 NULL,
+	id_session int4 NULL,
+	plase int4 NULL,
+	"cost" float4 NULL,
+	CONSTRAINT tickets_pkey PRIMARY KEY (id),
+	CONSTRAINT tickets_fk FOREIGN KEY (id_client) REFERENCES clients(id) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT tickets_fk_1 FOREIGN KEY (id_session) REFERENCES sessions(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
--- hw5/sqript.sql
+++ hw5/sqript.sql
@@ -1,9 +1,9 @@
-SELECT MaxName, MaxSum
+CREATE VIEW max_price AS SELECT MaxName, MaxSum
 FROM 
 (
-    SELECT films.name as MaxName, SUM(orders.count) * films.price as MaxSum
-    FROM orders 
-	JOIN sessions ON orders.id_session = sessions.id
+    SELECT films.name as MaxName, SUM(tickets.cost) as MaxSum
+    FROM tickets 
+	JOIN sessions ON tickets.id_session = sessions.id
 	JOIN films ON sessions.id_film = films.id
 	GROUP BY films.id
     order by MaxSum desc
