6ace8c66cbb7a1d4165fa8e79816fc68bfeb0923
NightWizard75 <vladimir@valyakin.ru>
2021-04-07 00:31:16 +0300

    HomeWork: Block-DataBase, Lesson 10

--- db/postgres/createViews/movie_task.sql
+++ db/postgres/createViews/movie_task.sql
@@ -1,16 +1,16 @@
--- Задачи
+-- Р—Р°РґР°С‡Рё
 create view movie_task as 
 select m."name", 
 		(case 
 		when av.value_date = CURRENT_DATE
 	 	   		then value_text
 	 	else '-'
-		end) as "Задачи на сегодня",
+		end) as "Р—Р°РґР°С‡Рё РЅР° СЃРµРіРѕРґРЅСЏ",
 		(case 
 		when av.value_date = CURRENT_DATE + interval '20' day
 	 	   		then value_text
 	 	else '-'
-		end) as "Выполнить через 20 дней"
+		end) as "Р’С‹РїРѕР»РЅРёС‚СЊ С‡РµСЂРµР· 20 РґРЅРµР№"
 from attribute_value av 
 left join "attribute" a on a.id = av.attribute_id 
 left join movie m on m.id = av.movie_id
