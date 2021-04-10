61b8fb12c18cf8647697f3d4bc765979552ef137
Vladimir Nikitin <v.nikitin@squesito.ru>
2021-04-10 13:29:49 +0300

    added files

---
+++ app/public/app.php
@@ -0,0 +1,15 @@
+<?php
+require "../vendor/autoload.php"; 
+
+use Application\App;
+
+try {
+    $app = new App();
+    $app->run();
+}
+
+catch(Exception $e){
+    echo 'Ошибка: ',  $e->getMessage(), "\n";
+}
+
+
---
+++ app/src/Application/App.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace Application;
+
+use Exception;
+
+class App 
+{
+    private $validator;
+
+    public function __construct()
+    {
+       
+    }
+
+    public function run()
+    {
+        echo "It works!";
+    }
+}
