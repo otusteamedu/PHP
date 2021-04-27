cf228b43fb1156c798a58a9296d93859c21690db
aivanov <a3bg5@protonmail.com>
2021-04-27 14:55:23 +0300

    rename parent folder to add hw 12

--- hw11-12/index.php
+++ hw11-12/index.php
@@ -5,5 +5,5 @@ require_once('./config/bootstrap.php');
 try {
     runApplication();
 } catch (\Exception $e) {
-    var_dump('Exception caught at index.php:' . $e->getMessage());
+    echo ('Exception caught at index.php:' . $e->getMessage() . PHP_EOL);
 }
