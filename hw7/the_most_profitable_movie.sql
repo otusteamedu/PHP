590c208d742c1dbb895ff6d98f906d1aab934c0b
Dmitriy Korytnyi <dmitriy.korytnyi@optimusite.com>
2021-06-02 13:46:02 +0300

    DKorytnyi/hw7 Refactored entities

--- hw7/the_most_profitable_movie.sql
+++ hw7/the_most_profitable_movie.sql
@@ -1,10 +1,10 @@
 select
     m.id,
     m.name,
-    SUM(s.price) as price
+    SUM(t.total_price) as profit
 from movies m
 inner join sessions s on s.movie_id = m.id
 inner join tickets t on t.session_id = s.id
 group by m.id, m.name
-order by price desc
+order by profit desc
 limit 0, 1
