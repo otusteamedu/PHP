56d37e5841c33188c998e626fa0176266a263ded
yessaliyev <40271311+yessaliyev@users.noreply.github.com>
2021-07-06 16:29:33 +0600

    docker and homestead

--- docker/app/public/index.php
+++ docker/app/public/index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo 'hello';
+try {
+    $mc = new Memcached();
+    $mc->addServer("memcached", 11211);
+
+    $redis = new Redis();
+    $redis->connect('redis');
+
+} catch (\Throwable $e) {
+    echo $e->getMessage();
+}
