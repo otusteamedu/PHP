8f410fec80d87cde9116a462ebde68276853f33e
Vladimir Valyakin <v.valyakin@webtrade.su>
2021-07-09 01:10:11 +0300

    Block-DataBase, Lesson 11: Start project

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\event;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EventFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = event::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
