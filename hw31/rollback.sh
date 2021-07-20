5124bb840de6002febb4d9bcf9aea873f1f8f6a1
kaivladimirv <kaivladimirv@gmail.com>
2021-07-20 13:13:17 +0600

    hw31(refactor): Удалить установку и изменение файла конфигурации nginx.conf

--- hw31/rollback.sh
+++ hw31/rollback.sh
@@ -17,11 +17,8 @@ then
 fi
 
 sudo cp -f ./$previousReleaseDir/supervisor.conf /etc/supervisor/conf.d/mysite.conf
-sudo cp -f /$previousReleaseDir/nginx.conf /etc/nginx/conf.d/mysite.conf
-sudo sed -i -- "s|%SERVER_NAME%|$1|g" /etc/nginx/conf.d/mysite.conf
 
 sudo ln -sfn ./$previousReleaseDir ./current
 
-sudo service nginx reload
 sudo service php7.4-fpm reload
 sudo supervisorctl update all
